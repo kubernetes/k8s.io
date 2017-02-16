4f6eeaca244cd646d8bf59b904a2a8085141a655
Jeff Grafton <jgrafton@google.com>
2017-02-16 13:35:34 -0800

    Add redirect for go.k8s.io/oncall

--- k8s.io/test.py
+++ k8s.io/test.py
@@ -93,6 +93,9 @@ class RedirTest(unittest.TestCase):
             self.assert_temp_redirect(base + 'help-wanted',
                 'https://github.com/kubernetes/kubernetes/labels/help-wanted')
             self.assert_temp_redirect(
+                base + 'oncall',
+                'https://storage.googleapis.com/kubernetes-jenkins/oncall.html')
+            self.assert_temp_redirect(
                 base + 'partner-request',
                 'https://docs.google.com/forms/d/e/1FAIpQLSdN1KtSKX2VAOPGABFlShkSd6CajQynoL4QCVtY0dj76MNDKg/viewform')
             self.assert_temp_redirect(base + 'start',
