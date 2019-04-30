53a487857a7d3803e67ffa5cfb9fcc3b158f16ee
Christoph Blecker <admin@toph.ca>
2019-04-30 00:19:03 -0700

    Add Makefile and fix up check/push scripts

--- dns/check-zone.sh
+++ dns/check-zone.sh
@@ -1,17 +1,48 @@
 #!/usr/bin/env bash
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # This runs as you.  It assumes you have built an image named ${USER}/octodns.
 # It also requires working gcloud credentials
 #
 # $1: fqdn including final dot... ex "canary.k8s.io."
-USAGE="Usage: $0 zone.in.gcloud."
 
-if [ "$#" != "1" ]; then
-	  echo "$USAGE"
-	  exit 1
-fi
-DOMAIN=$1
-echo "Checking that the GCP dns servers for $DOMAIN serve up everything in our octodns config"
+read -r -d '' USAGE <<- EOF
+  Usage:
+  $0 example.com.\t\t\t# Check a single zone
+  $0 example.com. example.io.\t# Check a multiple zones
+EOF
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+      echo -e "${USAGE}"
+      exit 0
+      ;;
+    \? )
+    echo -e "Invalid option.\n" >&2
+    echo -e "${USAGE}"
+    exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+DOMAINS=("$@")
+
+echo "Checking that the GCP dns servers for (${DOMAINS[@]}) serve up everything in our octodns config"
 docker run -ti \
        -u `id -u` \
        -v ~/.config/gcloud:/.config/gcloud:ro \
@@ -20,7 +51,7 @@ docker run -ti \
        ${USER}/octodns \
        check-zone \
        --config-file=/octodns/config.yaml \
-       --zone $1
+       ${DOMAINS[@]/#/--zone=}
 RESULT=$?
 if [ $RESULT != "0" ] ; then
     echo '***FAIL***'
