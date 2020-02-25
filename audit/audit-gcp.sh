3173e850c7e61af6f23cd0d8ed35b75dec07fd3b
Tim Hockin <thockin@google.com>
2020-02-24 21:43:46 -0800

    Another pass at audit cleanup
    This cleans up the script and the output, and audits a few more things
    like service-account IAM.

--- audit/audit-gcp.sh
+++ audit/audit-gcp.sh
@@ -6,96 +6,151 @@ set -o pipefail
 
 CNCF_GCP_ORG=758905017065
 
-# gcloud organizations describe ${CNCF_GCP_ORG} 2>&1
-# ERROR: (gcloud.organizations.describe)
-# User [hh@ii.coop] does not have permission to access organization []
-
-format=json
-
 echo "# Auditing CNCF CGP Org: ${CNCF_GCP_ORG} #"
 mkdir -p org_kubernetes.io/roles
-for ROLE_PATH in $(gcloud iam roles list --organization="${CNCF_GCP_ORG}" --format="value(name)")
-do
+gcloud \
+    iam roles list \
+    --organization="${CNCF_GCP_ORG}" \
+    --format="value(name)" \
+| while read ROLE_PATH; do
     ROLE=$(basename "${ROLE_PATH}")
-    gcloud iam roles describe --organization="${CNCF_GCP_ORG}" "${ROLE}" \
-        --format=json > "org_kubernetes.io/roles/${ROLE}.json"
+    gcloud iam roles describe "${ROLE}" \
+        --organization="${CNCF_GCP_ORG}" \
+        --format=json \
+        > "org_kubernetes.io/roles/${ROLE}.json"
 done
-gcloud organizations get-iam-policy "${CNCF_GCP_ORG}" --format="${format}" \
-       > "org_kubernetes.io/iam.${format}"
+gcloud \
+    organizations get-iam-policy "${CNCF_GCP_ORG}" \
+    --format=json \
+    > "org_kubernetes.io/iam.json"
 
 echo "## Iterating over Projects ##"
-gcloud projects list \
-       --filter "parent.id=${CNCF_GCP_ORG}" \
-       --format "value(name, projectNumber)" \
-    | sort \
-    | while read PROJECT NUM; do
+gcloud \
+    projects list \
+    --filter="parent.id=${CNCF_GCP_ORG}" \
+    --format="value(name, projectNumber)" \
+| sort \
+| while read PROJECT NUM; do
     export CLOUDSDK_CORE_PROJECT="${PROJECT}"
-    echo "### Auditing Project: ${PROJECT} ###"
+
+    echo "### Auditing Project ${PROJECT} ###"
     mkdir -p "${PROJECT}"
-    gcloud projects get-iam-policy "${PROJECT}" --format="${format}" > "${PROJECT}/iam.${format}"
-    mkdir -p "${PROJECT}/roles"
-    for ROLE_PATH in $(gcloud iam roles list --project "${PROJECT}" --format="value(name)")
-    do
-        ROLE=$(basename "${ROLE_PATH}")
-        gcloud iam roles --project="${PROJECT}" describe "${ROLE}" \
-               --format=json > "${PROJECT}/roles/${ROLE}.json"
+    gcloud \
+        projects describe "${PROJECT}" \
+        --format=json \
+        > "${PROJECT}/description.json"
+
+    echo "#### ${PROJECT} IAM ####"
+    gcloud \
+        projects get-iam-policy "${PROJECT}" \
+        --format=json \
+        > "${PROJECT}/iam.json"
+
+    echo "#### ${PROJECT} ServiceAccounts ####"
+    gcloud \
+        iam service-accounts list \
+        --project="${PROJECT}" \
+        --format="value(email)" \
+    | while read SVCACCT; do
+        mkdir -p "${PROJECT}/service-accounts/${SVCACCT}"
+        gcloud \
+            iam service-accounts describe "${SVCACCT}" \
+            --project="${PROJECT}" \
+            --format=json \
+            > "${PROJECT}/service-accounts/${SVCACCT}/description.json"
+        gcloud \
+            iam service-accounts get-iam-policy "${SVCACCT}" \
+            --project="${PROJECT}" \
+            --format=json \
+            > "${PROJECT}/service-accounts/${SVCACCT}/iam.json"
     done
 
-    echo "#### Iterating over ${PROJECT} Services: ####"
-    mkdir -p "${PROJECT}/services"
-    gcloud services list --filter state:ENABLED --format="${format}" > "${PROJECT}/services/enabled.${format}"
-    for service in $(gcloud services list --filter state:ENABLED --format=json \
-                    | jq -r .[].config.name | sed s:.googleapis.com:: \
-                    | sort)
-    do
-        case "${service}" in
+    echo "#### ${PROJECT} Roles ####"
+    gcloud \
+        iam roles list \
+        --project="${PROJECT}" \
+        --format="value(name)" \
+    | while read ROLE_PATH; do
+        mkdir -p "${PROJECT}/roles"
+        ROLE=$(basename "${ROLE_PATH}")
+        gcloud \
+            iam roles describe "${ROLE}" \
+            --project="${PROJECT}" \
+            --format=json \
+            > "${PROJECT}/roles/${ROLE}.json"
+    done
+
+    echo "#### Services ####"
+    gcloud \
+        services list \
+        --filter="state:ENABLED" \
+        --format=json \
+        > "${PROJECT}/services/enabled.json"
+    gcloud \
+        services list \
+        --filter="state:ENABLED" \
+        --format="value(config.name)" \
+    | sed 's/.googleapis.com//' \
+    | sort \
+    | while read SVC; do
+        case "${SVC}" in
             compute)
-                echo "TODO: ${service} Needs compute.projects.get"
-                #### gcloud compute project-info describe
-                #### gcloud compute instances list --format="${format}" > "${PROJECT}/services/compute.instances.${format}"
-                #### gcloud compute disks list --format="${format}" > "${PROJECT}/services/compute.disks.${format}"
+                mkdir -p "${PROJECT}/services/${SVC}"
+                gcloud \
+                    compute project-info describe \
+                    --project="${PROJECT}" \
+                    --format=json \
+                    > "${PROJECT}/services/${SVC}/project-info.json"
                 ;;
             container)
-                echo "${service}"
+                mkdir -p "${PROJECT}/services/${SVC}"
                 # Don't do a JSON dump here - too much changes without human
                 # action.
-                mkdir -p "${PROJECT}/services/container"
-                gcloud container clusters list > "${PROJECT}/services/container/clusters.txt"
+                gcloud \
+                    container clusters list \
+                    --format="value(name, location, locations, currentNodeCount, status)" \
+                    > "${PROJECT}/services/${SVC}/clusters.txt"
                 ;;
             dns)
-                echo "${service}"
-                mkdir -p "${PROJECT}/services/dns"
-                gcloud dns project-info describe "${PROJECT}" --format="${format}" > "${PROJECT}/services/dns/info.${format}"
-                gcloud dns managed-zones list --format="${format}" > "${PROJECT}/services/dns/zones.${format}"
+                mkdir -p "${PROJECT}/services/${SVC}"
+                gcloud \
+                    dns project-info describe "${PROJECT}" \
+                    --format=json \
+                    > "${PROJECT}/services/${SVC}/info.json"
+                gcloud \
+                    dns managed-zones list \
+                    --format=json \
+                    > "${PROJECT}/services/${SVC}/zones.json"
                 ;;
             logging)
-                echo "TODO: ${service} needs serviceusage.services.use"
-                ##### gcloud logging logs list --format="${format}" > "${PROJECT}/services/logging.logs.${format}"
-                ##### gcloud logging metrics list --format="${format}" > "${PROJECT}/services/logging.metrics.${format}"
-                ##### gcloud logging sinks list --format="${format}" > "${PROJECT}/services/logging.sinks.${format}"
+                echo "TODO: ${SVC} needs serviceusage.services.use"
+                ##### gcloud logging logs list --format=json > "${PROJECT}/services/logging.logs.json"
+                ##### gcloud logging metrics list --format=json > "${PROJECT}/services/logging.metrics.json"
+                ##### gcloud logging sinks list --format=json > "${PROJECT}/services/logging.sinks.json"
                 ;;
             monitoring)
-                echo "TODO: ${service} needs serviceusage.services.use"
-                #### gcloud alpha monitoring policies list > "${PROJECT}/services/monitoring.policies.${format}"
-                #### gcloud alpha monitoring channels list > "${PROJECT}/services/monitoring.channels.${format}"
-                #### gcloud alpha monitoring channel-descriptors list > "${PROJECT}/services/monitoring.channel-descriptors.${format}"
+                echo "TODO: ${SVC} needs serviceusage.services.use"
+                #### gcloud alpha monitoring policies list > "${PROJECT}/services/monitoring.policies.json"
+                #### gcloud alpha monitoring channels list > "${PROJECT}/services/monitoring.channels.json"
+                #### gcloud alpha monitoring channel-descriptors list > "${PROJECT}/services/monitoring.channel-descriptors.json"
                 ;;
             storage-api)
-                echo "${service}"
-                for BUCKET in $(gsutil ls -p "${PROJECT}" | awk -F/ '{print $3}')
-                do
+                gsutil ls -p "${PROJECT}" \
+                | awk -F/ '{print $3}' \
+                | while read BUCKET; do
                     mkdir -p "${PROJECT}/buckets/${BUCKET}"
-                    gsutil bucketpolicyonly get "gs://${BUCKET}/" > "${PROJECT}/buckets/${BUCKET}/bucketpolicyonly.json"
-                    gsutil cors get "gs://${BUCKET}/" > "${PROJECT}/buckets/${BUCKET}/cors.json"
-                    gsutil logging get "gs://${BUCKET}/" > "${PROJECT}/buckets/${BUCKET}/logging.json"
-                    gsutil iam get "gs://${BUCKET}/" > "${PROJECT}/buckets/${BUCKET}/iam.json"
+                    gsutil bucketpolicyonly get "gs://${BUCKET}/" \
+                        > "${PROJECT}/buckets/${BUCKET}/bucketpolicyonly.json"
+                    gsutil cors get "gs://${BUCKET}/" \
+                        > "${PROJECT}/buckets/${BUCKET}/cors.json"
+                    gsutil logging get "gs://${BUCKET}/" \
+                        > "${PROJECT}/buckets/${BUCKET}/logging.json"
+                    gsutil iam get "gs://${BUCKET}/" \
+                        > "${PROJECT}/buckets/${BUCKET}/iam.json"
                 done
                 ;;
-            storage-component)
-                echo "${service}"
-                ;;
             *)
-                echo "# Unhandled Service ${service} #"
+                echo "# Unhandled Service ${SVC} #"
                 ;;
         esac
     done
@@ -104,5 +159,3 @@ done
 
 # TODO:
 # Dump iam for Big Query
-# Iterate over enabled APIs per project
-# Identify each resource, then dump iam
