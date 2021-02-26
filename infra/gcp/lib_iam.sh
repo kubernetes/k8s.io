7b3022dc6a17e044466cc6d6772b5ac81b33daca
Aaron Crickenberger <spiffxp@google.com>
2021-02-26 05:57:55 -0500

    infra/gcp: fix custom_iam_role_from_file
    need to substitute in full role name depending on whether creating as
    project role or organization role
    
    output diff of role

--- infra/gcp/lib_iam.sh
+++ infra/gcp/lib_iam.sh
@@ -76,26 +76,39 @@ function ensure_custom_iam_role_from_file() {
     local scope="${1}"
     local name="${2}"
     local file="${3}"
+    local full_name="${name}"
 
     scope_flag=""
     if [[ "${scope}" == "org" ]]; then
         scope_flag="--organization ${GCP_ORG}" 
+        full_name="organizations/${GCP_ORG}/roles/${name}"
     elif [[ "${scope}" =~ "^project:" ]]; then
-        scope_flag="--project $(echo ${scope} | cut -d: -f2-)"
+        project=$(echo "${scope}" | cut -d: -f2-)
+        scope_flag="--project ${project}"
+        full_name="projects/${project}/roles/${name}"
     else
         echo "ensure_custom_iam_role_from_file(scope, name, file) scope must be one of 'org' or 'project:project-id'" >&2
         return 1
     fi
 
-    if ! gcloud iam roles describe ${scope_flag} "${name}" \
-        >/dev/null 2>&1
-    then
-        # be noisy when creating a role
-        gcloud iam roles create ${scope_flag} "${name}" --file "${file}"
-    else
-        # be quiet when updating, only output name of role
-        gcloud iam roles update ${scope_flag} "${name}" --file "${file}" | grep ^name:
+    tmp_dir=$(mktemp -d "/tmp/ensure-role-${name}-XXXXX")
+    trap 'rm -rf "${tmp_dir}"' EXIT
+    before="${tmp_dir}/before.${role}.yaml"
+    ready="${tmp_dir}/ready.${role}.yaml"
+    after="${tmp_dir}/after.${role}.yaml"
+
+    # detect if we should create or update and dump role; silently ignore error
+    verb="update"
+    if ! (gcloud iam roles describe ${scope_flag} "${name}" >"${before}") >/dev/null 2>&1; then
+      verb="create"
     fi
+
+    # name is foo.bar, but gcloud wants scope/id/role/foo.bar in the file
+    <"${file}" sed -e "s|^name: ${name}|name: ${full_name}|" >"${ready}"
+    gcloud iam roles "${verb}" ${scope_flag} "${name}" --file "${ready}" > "${after}"
+
+    # if they differ, ignore the error
+    diff "${before}" "${after}" || true
 }
 
 # Return the full name of a custom IAM role defined at the org level
