a2a3fb679155fa645826a88ea3606a9a94b42977
Aaron Crickenberger <spiffxp@google.com>
2021-03-31 12:01:42 -0400

    infra/gcp/lib: add ensure_only_services
    The goal is ensure that only services we have specified in k8s.io
    source code are enabled.  This means disabling any service that either
    wasn't directly specified, or isn't a direct dependency of services that
    were directly specified.
    
    This isn't perfect:
    - it's bash+jq operating on a graph, what do you expect
    - this does a single add/remove pass, it could be more are necessary
    - this traverses service names alphabetically, not in dependency-order,
      so it uses --force when disabling services; this is scary, so unless
      we set an obnoxiously long env var, disable commands are just printed
    
    NOTE: set K8S_INFRA_ENSURE_ONLY_SERVICES_WILL_FORCE_DISABLE=true to
    actually disable services, may still require human attention

--- infra/gcp/lib.sh
+++ infra/gcp/lib.sh
@@ -601,3 +601,101 @@ function ensure_regional_address() {
         --region="${region}"
     fi
 }
+
+# Output a plan of services to enable/disable for a given gcp project; format
+# is YAML, each key is a list of services e.g. [pubsub.googleapis.com, ...]
+#   intent:     # services we wish to enable
+#   enabled:    # services that are presently enabled
+#   expected:   # intent + any services directly depended on by intent
+#   to_enable:  # services in intent that are not enabled
+#   to_disable: # services that are enabled but not in expected
+# $1  The GCP project
+# $2+ Service names that are expected to be enabled (e.g. pubsub.googleapis.com)
+function _plan_enabled_services() {
+    if [ $# -lt 2 -o -z "$1" ]; then
+        echo "list_services(gcp_project, service...) requires at least 2 arguments" >&2
+        return 1
+    fi
+
+    local gcp_project="$1"; shift
+
+    gcloud services list --enabled --project="${gcp_project}" \
+      --format='yaml(config.name,dependencyConfig.directlyDependsOn)' \
+      | yq --slurp -y --args '($ARGS.positional | sort) as $intent | {
+        intent: $intent,
+        enabled: map(.config.name) | sort,
+        expected: (
+          map(
+            select([.config.name] | inside($intent))
+            | (.dependencyConfig?.directlyDependsOn // [])
+          ) + $intent
+        )
+        | flatten
+        | map({key:., value:true}) | from_entries | keys | sort
+      } | . += {
+        to_enable: (.expected - .enabled),
+        to_disable: (.enabled - .expected)
+      }' "$@"
+}
+
+# Ensure that only the given services and their direct dependencies are enabled; disable any other services
+# $1  The GCP project for which to enable/disable services
+# $2+ The service names (e.g. pubsub.googleapis.com)
+function ensure_only_services() {
+    if [ $# -lt 2 -o -z "$1" -o -z "$2" ]; then
+        echo "ensure_only_services(gcp_project, service...) requires at least 2 arguments" >&2
+        return 1
+    fi
+
+    local gcp_project="$1"; shift
+
+    local tmp_dir
+    tmp_dir=$(mktemp -d "/tmp/k8sio-infra-gcp-lib.XXXXX")
+    # tmp_dir is local but trap is global, so expand now to avoid unbound variable on exit
+    # shellcheck disable=SC2064
+    trap "rm -rf ${tmp_dir}" EXIT
+
+    local before="${tmp_dir}/ensure-only-services.before.yaml"
+    local after_enable="${tmp_dir}/ensure-only-services.after_enable.yaml"
+    local after_disable="${tmp_dir}/ensure-only-services.after_disable.yaml"
+
+    # get services before modifying to diff against when finished
+    _plan_enabled_services "${gcp_project}" "$@" > "${before}"
+
+    # if there's nothing to do, return early
+    if ! <"${before}" yq --exit-status '[.to_enable, .to_disable] | map (length > 0) | any' >/dev/null; then
+      return
+    fi
+
+    echo "plan to enable/disable the following services"
+    <"${before}" yq -y '{to_enable, to_disable}'
+
+    # enable services that need to be enabled
+    for service in $(<"${before}" yq -r '.to_enable[]'); do
+        gcloud services enable --project="${gcp_project}" "${service}"
+    done
+
+    # disable services not explicitly enabled or directly required
+    _plan_enabled_services "${gcp_project}" "$@" > "${after_enable}"
+
+
+    # TODO(spiffxp): get comfortable with --force or redo to disable in dep-order;
+    #                until then, set an obnoxiously long env var to actually disable
+    local disable_cmd='echo "INFO: dry-run mode, would run:" gcloud'
+    if [ "${K8S_INFRA_ENSURE_ONLY_SERVICES_WILL_FORCE_DISABLE:-""}" == "true" ]; then
+        disable_cmd=gcloud
+    fi
+    for service in $( (<"${after_enable}" yq -r '.to_disable[]') | sort | uniq -u); do
+        ${disable_cmd} services disable --force "${service}" --project="${gcp_project}"
+    done
+
+    _plan_enabled_services "${gcp_project}" "$@" > "${after_disable}"
+
+    # in the event that an enable/disable cycle doesn't do enough, let's warn
+    if <"${after_disable}" yq --exit-status '[.to_enable, .to_disable] | map (length > 0) | any' >/dev/null; then
+      echo "WARN: ensure_only_services: after enable/disable cycle, still projects to enable/disable: ${gcp_project}"
+      cat "${after_disable}"
+    fi
+
+    diff_colorized "${before}" "${after_disable}"
+}
