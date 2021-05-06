65792aef34904e4d1f4f67d107fd205f92d7cf24
Arnaud Meukam <ameukam@gmail.com>
2021-05-06 02:07:39 +0200

    Hardcoded versions of terraform fields
    Some fields or attributes of resources have changed in the version 3 of the
    provider. This force the `aaa` cluster to be recreated.
    We want to avoid this by setting static values of those fields.
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/clusters/projects/kubernetes-public/aaa/10-cluster-configuration.tf
+++ infra/gcp/clusters/projects/kubernetes-public/aaa/10-cluster-configuration.tf
@@ -15,24 +15,24 @@ locals {
 
 // Create SA for nodes
 resource "google_service_account" "cluster_node_sa" {
-  project      = data.google_project.project.id
+  project      = "kubernetes-public"
   account_id   = "gke-nodes-${local.cluster_name}"
   display_name = "Nodes in GKE cluster '${local.cluster_name}'"
 }
 
 // Add roles for SA
 resource "google_project_iam_member" "cluster_node_sa_logging" {
-  project = data.google_project.project.id
+  project = "kubernetes-public"
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.cluster_node_sa.email}"
 }
 resource "google_project_iam_member" "cluster_node_sa_monitoring_viewer" {
-  project = data.google_project.project.id
+  project = "kubernetes-public"
   role    = "roles/monitoring.viewer"
   member  = "serviceAccount:${google_service_account.cluster_node_sa.email}"
 }
 resource "google_project_iam_member" "cluster_node_sa_monitoring_metricwriter" {
-  project = data.google_project.project.id
+  project = "kubernetes-public"
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.cluster_node_sa.email}"
 }
@@ -40,7 +40,7 @@ resource "google_project_iam_member" "cluster_node_sa_monitoring_metricwriter" {
 // BigQuery dataset for usage data
 resource "google_bigquery_dataset" "usage_metering" {
   dataset_id  = replace("usage_metering_${local.cluster_name}", "-", "_")
-  project     = data.google_project.project.id
+  project     = "kubernetes-public"
   description = "GKE Usage Metering for cluster '${local.cluster_name}'"
   location    = local.bigquery_location
 
@@ -64,7 +64,7 @@ resource "google_container_cluster" "cluster" {
   location = local.cluster_location
 
   provider = google-beta
-  project  = data.google_project.project.id
+  project  = "kubernetes-public"
 
   // GKE clusters are critical objects and should not be destroyed
   // IMPORTANT: should be false on test clusters
@@ -75,8 +75,8 @@ resource "google_container_cluster" "cluster" {
   // Network config
   network = "default"
   ip_allocation_policy {
-    use_ip_aliases    = true
-    create_subnetwork = true
+    cluster_ipv4_cidr_block  = "10.40.0.0/14"
+    services_ipv4_cidr_block = "10.107.16.0/20"
   }
 
   // Start with a single node, because we're going to delete the default pool
@@ -96,6 +96,11 @@ resource "google_container_cluster" "cluster" {
     }
   }
 
+  // Release Channel subscriptions. See https://cloud.google.com/kubernetes-engine/docs/concepts/release-channels
+  release_channel {
+    channel = "STABLE"
+  }
+
   // Enable google-groups for RBAC
   authenticator_groups_config {
     security_group = "gke-security-groups@kubernetes.io"
@@ -103,7 +108,7 @@ resource "google_container_cluster" "cluster" {
 
   // Enable workload identity for GCP IAM
   workload_identity_config {
-    identity_namespace = "${data.google_project.project.id}.svc.id.goog"
+    identity_namespace = "kubernetes-public.svc.id.goog"
   }
 
   // Enable Stackdriver Kubernetes Monitoring
@@ -153,12 +158,12 @@ resource "google_container_cluster" "cluster" {
     enabled = true
     resource_limits {
       resource_type = "cpu"
-      minimum = 2
-      maximum = 64
+      minimum       = 2
+      maximum       = 64
     }
     resource_limits {
       resource_type = "memory"
-      maximum = 256
+      maximum       = 256
     }
   }
 
