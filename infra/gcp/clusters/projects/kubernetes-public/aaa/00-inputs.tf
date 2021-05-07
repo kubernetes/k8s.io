4a9b784f4b053bc108988c3a19de59f2dcc10981
Aaron Crickenberger <spiffxp@google.com>
2021-05-07 13:17:12 -0400

    infra/gcp/clusters: bump google provider version

--- infra/gcp/clusters/projects/kubernetes-public/aaa/00-inputs.tf
+++ infra/gcp/clusters/projects/kubernetes-public/aaa/00-inputs.tf
@@ -7,7 +7,7 @@ This file defines:
 */
 
 terraform {
-  required_version = "~> 0.13.6"
+  required_version = "~> 0.13.0"
 
   backend "gcs" {
     bucket = "k8s-infra-tf-public-clusters"
@@ -16,10 +16,10 @@ terraform {
 
   required_providers {
     google = {
-      version = "~> 3.46.0"
+      version = "~> 3.66.0"
     }
     google-beta = {
-      version = "~> 3.46.0"
+      version = "~> 3.66.0"
     }
   }
 }
