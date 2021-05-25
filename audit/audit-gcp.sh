7668dbf97e93ecce5b41760dbd97c0939c3257d2
Aaron Crickenberger <spiffxp@gmail.com>
2021-05-25 12:45:11 -0400

    audit: refactor into functions
    mechanical refactor into functions:
    
    - remove_all_gcp_project_audit_files
    - audit_gcp_organization
    - audit_all_gcp_projects_with_parent_id
    - audit_gcp_project
    - audit_gcp_project_service
    - main
    
    ensure each function removes whichever folder it is about to populate,
    instead of deleting everything up front (to support targeted audits),
    except for audit_all_gcp_projects_with_parent_id, since it's populating
    a flat/non-hiearchical folder that may contain projects with other
    parent id's
    
    touch up logging for consistency

--- audit/audit-gcp.sh
+++ audit/audit-gcp.sh
@@ -18,59 +18,90 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CNCF_GCP_ORG=758905017065
+readonly CNCF_GCP_ORG=758905017065
 
 function format_gcloud_json() {
-  # recursively delete any fields named "etag"
-  jq 'delpaths([path(..|.etag?|select(.))])'
+    # recursively delete any fields named "etag"
+    jq 'delpaths([path(..|.etag?|select(.))])'
 }
 
-echo "# Removing existing audit files"
-rm -rf org_kubernetes.io
-rm -rf projects
+function remove_all_gcp_project_audit_files() {
+    echo "# Removing existing audit files"
+    rm -rf projects
+}
 
-echo "# Auditing CNCF CGP Org: ${CNCF_GCP_ORG}"
-mkdir -p org_kubernetes.io/roles
-gcloud \
-    iam roles list \
-    --organization="${CNCF_GCP_ORG}" \
-    --format="value(name)" \
-| while read -r ROLE_PATH; do
-    ROLE=$(basename "${ROLE_PATH}")
-    gcloud iam roles describe "${ROLE}" \
-        --organization="${CNCF_GCP_ORG}" \
+function audit_gcp_organization() {
+    local organization="${1}"
+    echo "# Auditing CGP Org: ${organization}"
+
+    echo "## Removing existing audit files for organization: ${organization}"
+    rm -rf org_kubernetes.io
+
+    echo "## Exporting IAM roles for organization: ${organization}"
+    rm -rf org_kubernetes.io/roles
+    mkdir -p org_kubernetes.io/roles
+    gcloud \
+        iam roles list \
+        --organization="${organization}" \
+        --format="value(name)" \
+    | while read -r ROLE_PATH; do
+        ROLE=$(basename "${ROLE_PATH}")
+        gcloud iam roles describe "${ROLE}" \
+            --organization="${organization}" \
+            --format=json | format_gcloud_json \
+            > "org_kubernetes.io/roles/${ROLE}.json"
+    done
+
+    echo "## Exporting IAM policy for org: ${organization}"
+    rm -rf org_kubernetes.io/iam.json
+    gcloud \
+        organizations get-iam-policy "${organization}" \
         --format=json | format_gcloud_json \
-        > "org_kubernetes.io/roles/${ROLE}.json"
-done
-gcloud \
-    organizations get-iam-policy "${CNCF_GCP_ORG}" \
-    --format=json | format_gcloud_json \
-    > "org_kubernetes.io/iam.json"
+        > "org_kubernetes.io/iam.json"
 
-echo "## Iterating over Projects"
-gcloud \
-    projects list \
-    --filter="parent.id=${CNCF_GCP_ORG}" \
-    --format="value(name, projectNumber)" \
-| sort \
-| while read -r PROJECT NUM; do
+}
+
+function audit_all_projects_with_parent_id() {
+    local parent_id="${1}"
+    echo "## Auditing all projects with parent id: ${parent_id}"
+    echo "## Removing existing audit files"
+    rm -rf projects
+    gcloud \
+        projects list \
+        --filter="parent.id=${parent_id}" \
+        --format="value(name, projectNumber)" \
+    | sort \
+    | while read -r PROJECT NUM; do
+        audit_gcp_project "${PROJECT}" "${NUM}"
+    done
+}
+
+function audit_gcp_project() {
+    local PROJECT="${1}"
+    local NUM="${2}"
+
     export CLOUDSDK_CORE_PROJECT="${PROJECT}"
 
     echo "### Auditing Project ${PROJECT}"
+
+    echo "#### Removing existing audit files for project: ${PROJECT}"
+    rm -rf "projects/${PROJECT}"
+
     mkdir -p "projects/${PROJECT}"
 
+    echo "#### Exporting project description for project: ${PROJECT}"
     gcloud \
         projects describe "${PROJECT}" \
         --format=json | format_gcloud_json \
         > "projects/${PROJECT}/description.json"
 
-    echo "#### ${PROJECT} IAM"
+    echo "#### Exporting IAM policy for project: ${PROJECT}"
     gcloud \
         projects get-iam-policy "${PROJECT}" \
         --format=json | format_gcloud_json \
         > "projects/${PROJECT}/iam.json"
 
-    echo "#### ${PROJECT} ServiceAccounts"
+    echo "#### Exporting IAM serviceaccounts for project: ${PROJECT}"
     gcloud \
         iam service-accounts list \
         --project="${PROJECT}" \
@@ -89,7 +120,7 @@ gcloud \
             > "projects/${PROJECT}/service-accounts/${SVCACCT}/iam.json"
     done
 
-    echo "#### ${PROJECT} Roles"
+    echo "#### Exporting IAM roles for project: ${PROJECT}"
     gcloud \
         iam roles list \
         --project="${PROJECT}" \
@@ -104,147 +135,163 @@ gcloud \
             > "projects/${PROJECT}/roles/${ROLE}.json"
     done
 
-    echo "#### Services"
+    echo "#### Exporting enabled services for project: ${PROJECT}"
     mkdir -p "projects/${PROJECT}/services"
     gcloud \
         services list \
         --filter="state:ENABLED" \
         > "projects/${PROJECT}/services/enabled.txt"
+
+    echo "#### Exporting resources for all enabled services for project: ${PROJECT}"
     gcloud \
         services list \
         --filter="state:ENABLED" \
         --format="value(config.name)" \
     | sed 's/.googleapis.com//' \
     | while read -r SVC; do
-        echo "##### projects/${PROJECT}/services/${SVC}"
-        case "${SVC}" in
-            bigquery)
-                mkdir -p "projects/${PROJECT}/services/${SVC}"
+        echo "##### Exporting resources for service ${SVC} for project: ${PROJECT}"
+        audit_gcp_project_service "${PROJECT}" "${SVC}"
+    done
+}
+
+function audit_gcp_project_service() {
+    local PROJECT="${1}"
+    local SVC="${2}"
+    case "${SVC}" in
+        bigquery)
+            mkdir -p "projects/${PROJECT}/services/${SVC}"
+            bq \
+                ls \
+                --project_id="${PROJECT}" \
+                --format=json | format_gcloud_json \
+                > "projects/${PROJECT}/services/${SVC}/bigquery.datasets.json"
+            # Only run if there are any datasets
+            if [ -s "projects/${PROJECT}/services/${SVC}/bigquery.datasets.json" ]
+            then
                 bq \
                     ls \
                     --project_id="${PROJECT}" \
                     --format=json | format_gcloud_json \
-                    > "projects/${PROJECT}/services/${SVC}/bigquery.datasets.json"                
-                # Only run if there are any datasets
-                if [ -s "projects/${PROJECT}/services/${SVC}/bigquery.datasets.json" ]
-                then
-                    bq \
-                        ls \
-                        --project_id="${PROJECT}" \
-                        --format=json | format_gcloud_json \
-                        | jq -r '.[] | .datasetReference["datasetId"]' \
-                        | while read -r DATASET; do                        
-                            bq \
-                                show \
-                                --project_id="${PROJECT}" \
-                                --format=json \
-                                "${PROJECT}:${DATASET}" \
-                                | format_gcloud_json \
-                                | jq .access \
-                                > "projects/${PROJECT}/services/${SVC}/bigquery.datasets.${DATASET}.access.json"
-                        done
-                fi
-                ;;
-            compute)
-                mkdir -p "projects/${PROJECT}/services/${SVC}"
+                    | jq -r '.[] | .datasetReference["datasetId"]' \
+                    | while read -r DATASET; do
+                        bq \
+                            show \
+                            --project_id="${PROJECT}" \
+                            --format=json \
+                            "${PROJECT}:${DATASET}" \
+                            | format_gcloud_json \
+                            | jq .access \
+                            > "projects/${PROJECT}/services/${SVC}/bigquery.datasets.${DATASET}.access.json"
+                    done
+            fi
+            ;;
+        compute)
+            mkdir -p "projects/${PROJECT}/services/${SVC}"
+            gcloud \
+                compute project-info describe \
+                --project="${PROJECT}" \
+                --format=json | format_gcloud_json \
+                | jq 'del(.quotas[].usage, .commonInstanceMetadata.fingerprint)' \
+                > "projects/${PROJECT}/services/${SVC}/project-info.json"
+            ;;
+        container)
+            mkdir -p "projects/${PROJECT}/services/${SVC}"
+            # Don't do a JSON dump here - too much changes without human
+            # action.
+            gcloud \
+                container clusters list \
+                --format="value(name, location, locations, status)" \
+                > "projects/${PROJECT}/services/${SVC}/clusters.txt"
+            ;;
+        dns)
+            mkdir -p "projects/${PROJECT}/services/${SVC}"
+            gcloud \
+                dns project-info describe "${PROJECT}" \
+                --format=json | format_gcloud_json \
+                > "projects/${PROJECT}/services/${SVC}/info.json"
+            gcloud \
+                dns managed-zones list \
+                --format=json | format_gcloud_json \
+                > "projects/${PROJECT}/services/${SVC}/zones.json"
+            ;;
+        logging)
+            echo "TODO: ${SVC} needs serviceusage.services.use"
+            ##### gcloud logging logs list --format=json > "projects/${PROJECT}/services/logging.logs.json"
+            ##### gcloud logging metrics list --format=json > "projects/${PROJECT}/services/logging.metrics.json"
+            ##### gcloud logging sinks list --format=json > "projects/${PROJECT}/services/logging.sinks.json"
+            ;;
+        monitoring)
+            echo "TODO: ${SVC} needs serviceusage.services.use"
+            #### gcloud alpha monitoring policies list > "projects/${PROJECT}/services/monitoring.policies.json"
+            #### gcloud alpha monitoring channels list > "projects/${PROJECT}/services/monitoring.channels.json"
+            #### gcloud alpha monitoring channel-descriptors list > "projects/${PROJECT}/services/monitoring.channel-descriptors.json"
+            ;;
+        secretmanager)
+            gcloud \
+                secrets list \
+                --project="${PROJECT}" \
+                --format="value(name)" \
+            | while read -r SECRET; do
+                path="projects/${PROJECT}/secrets/${SECRET}"
+                mkdir -p "${path}"
                 gcloud \
-                    compute project-info describe \
+                    secrets describe "${SECRET}" \
                     --project="${PROJECT}" \
                     --format=json | format_gcloud_json \
-                    | jq 'del(.quotas[].usage, .commonInstanceMetadata.fingerprint)' \
-                    > "projects/${PROJECT}/services/${SVC}/project-info.json"
-                ;;
-            container)
-                mkdir -p "projects/${PROJECT}/services/${SVC}"
-                # Don't do a JSON dump here - too much changes without human
-                # action.
+                    > "${path}/description.json"
                 gcloud \
-                    container clusters list \
-                    --format="value(name, location, locations, status)" \
-                    > "projects/${PROJECT}/services/${SVC}/clusters.txt"
-                ;;
-            dns)
-                mkdir -p "projects/${PROJECT}/services/${SVC}"
-                gcloud \
-                    dns project-info describe "${PROJECT}" \
-                    --format=json | format_gcloud_json \
-                    > "projects/${PROJECT}/services/${SVC}/info.json"
-                gcloud \
-                    dns managed-zones list \
-                    --format=json | format_gcloud_json \
-                    > "projects/${PROJECT}/services/${SVC}/zones.json"
-                ;;
-            logging)
-                echo "TODO: ${SVC} needs serviceusage.services.use"
-                ##### gcloud logging logs list --format=json > "projects/${PROJECT}/services/logging.logs.json"
-                ##### gcloud logging metrics list --format=json > "projects/${PROJECT}/services/logging.metrics.json"
-                ##### gcloud logging sinks list --format=json > "projects/${PROJECT}/services/logging.sinks.json"
-                ;;
-            monitoring)
-                echo "TODO: ${SVC} needs serviceusage.services.use"
-                #### gcloud alpha monitoring policies list > "projects/${PROJECT}/services/monitoring.policies.json"
-                #### gcloud alpha monitoring channels list > "projects/${PROJECT}/services/monitoring.channels.json"
-                #### gcloud alpha monitoring channel-descriptors list > "projects/${PROJECT}/services/monitoring.channel-descriptors.json"
-                ;;
-            secretmanager)
-                gcloud \
-                    secrets list \
+                    secrets versions list "${SECRET}" \
                     --project="${PROJECT}" \
-                    --format="value(name)" \
-                | while read -r SECRET; do
-                    path="projects/${PROJECT}/secrets/${SECRET}"
-                    mkdir -p "${path}"
-                    gcloud \
-                        secrets describe "${SECRET}" \
-                        --project="${PROJECT}" \
-                        --format=json | format_gcloud_json \
-                        > "${path}/description.json"
-                    gcloud \
-                        secrets versions list "${SECRET}" \
-                        --project="${PROJECT}" \
-                        --format=json \
-                        > "${path}/versions.json"
-                    gcloud \
-                        secrets get-iam-policy "${SECRET}" \
-                        --project="${PROJECT}" \
-                        --format=json | format_gcloud_json \
-                        > "${path}/iam.json"
-                done
-                ;;
-            storage-api)
-                gsutil ls -p "${PROJECT}" \
-                | awk -F/ '{print $3}' \
-                | while read -r BUCKET; do
-                    mkdir -p "projects/${PROJECT}/buckets/${BUCKET}"
-                    gsutil bucketpolicyonly get "gs://${BUCKET}/" \
-                        > "projects/${PROJECT}/buckets/${BUCKET}/bucketpolicyonly.txt"
-                    gsutil cors get "gs://${BUCKET}/" \
-                        > "projects/${PROJECT}/buckets/${BUCKET}/cors.txt"
-                    gsutil logging get "gs://${BUCKET}/" \
-                        > "projects/${PROJECT}/buckets/${BUCKET}/logging.txt"
-                    gsutil iam get "gs://${BUCKET}/" \
-                        | format_gcloud_json \
-                        > "projects/${PROJECT}/buckets/${BUCKET}/iam.json"
-                done
-                ;;
-            *)
-                echo "WARN: Unaudited service enabled in project ${PROJECT}: ${SVC}"
-                # (these were all enabled for kubernetes-public)
-                # TODO: handle (or ignore) bigquerystorage
-                # TODO: handle (or ignore) clouderrorreporting
-                # TODO: handle (or ignore) cloudfunctions
-                # TODO: handle (or ignore) cloudresourcemanager
-                # TODO: handle (or ignore) cloudshell
-                # TODO: handle (or ignore) containerregistry
-                # TODO: handle (or ignore) iam
-                # TODO: handle (or ignore) iamcredentials
-                # TODO: handle (or ignore) oslogin
-                # TODO: handle (or ignore) pubsub
-                # TODO: handle (or ignore) serviceusage
-                # TODO: handle (or ignore) source
-                # TODO: handle (or ignore) stackdriver
-                # TODO: handle (or ignore) storage-component
-                ;;
-        esac
-    done
-done
+                    --format=json \
+                    > "${path}/versions.json"
+                gcloud \
+                    secrets get-iam-policy "${SECRET}" \
+                    --project="${PROJECT}" \
+                    --format=json | format_gcloud_json \
+                    > "${path}/iam.json"
+            done
+            ;;
+        storage-api)
+            gsutil ls -p "${PROJECT}" \
+            | awk -F/ '{print $3}' \
+            | while read -r BUCKET; do
+                mkdir -p "projects/${PROJECT}/buckets/${BUCKET}"
+                gsutil bucketpolicyonly get "gs://${BUCKET}/" \
+                    > "projects/${PROJECT}/buckets/${BUCKET}/bucketpolicyonly.txt"
+                gsutil cors get "gs://${BUCKET}/" \
+                    > "projects/${PROJECT}/buckets/${BUCKET}/cors.txt"
+                gsutil logging get "gs://${BUCKET}/" \
+                    > "projects/${PROJECT}/buckets/${BUCKET}/logging.txt"
+                gsutil iam get "gs://${BUCKET}/" \
+                    | format_gcloud_json \
+                    > "projects/${PROJECT}/buckets/${BUCKET}/iam.json"
+            done
+            ;;
+        *)
+            echo "WARN: Unaudited service ${SVC} enabled in project: ${PROJECT}"
+            # (these were all enabled for kubernetes-public)
+            # TODO: handle (or ignore) bigquerystorage
+            # TODO: handle (or ignore) clouderrorreporting
+            # TODO: handle (or ignore) cloudfunctions
+            # TODO: handle (or ignore) cloudresourcemanager
+            # TODO: handle (or ignore) cloudshell
+            # TODO: handle (or ignore) containerregistry
+            # TODO: handle (or ignore) iam
+            # TODO: handle (or ignore) iamcredentials
+            # TODO: handle (or ignore) oslogin
+            # TODO: handle (or ignore) pubsub
+            # TODO: handle (or ignore) serviceusage
+            # TODO: handle (or ignore) source
+            # TODO: handle (or ignore) stackdriver
+            # TODO: handle (or ignore) storage-component
+            ;;
+    esac
+}
+
+function main() {
+    remove_all_gcp_project_audit_files
+    audit_gcp_organization ${CNCF_GCP_ORG}
+    audit_all_projects_with_parent_id ${CNCF_GCP_ORG}
+}
+
+main
