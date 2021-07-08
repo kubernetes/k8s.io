4489f73889dfc125d586ec941d672c3dab8d44da
Jim DAgostino <dagosti@synopsys.com>
2021-07-07 22:47:56 -0400

    move slack-infra under apps

--- apps/slack-infra/deploy.sh
+++ apps/slack-infra/deploy.sh
@@ -15,7 +15,17 @@
 # limitations under the License.
 
 # Deploys slack-infra to the aaa cluster, or whatever cluster is pointed to
-# by KUBECTL_CONTEXT if set
+# by KUBECTL_CONTEXT if set. Assumes the app's namespace already exists.
+#
+# Members of k8s-infra-rbac-${app}@kubernetes.io can run this.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+
+app=$(basename "${SCRIPT_ROOT}")
 
 # coordinates to locate the target cluster in gke
 cluster_name="aaa"
@@ -25,12 +35,11 @@ cluster_region="us-central1"
 # coordinates to locate the secretmanager secrets to sync to kubernetes secrets
 secrets_project="${cluster_project}"
 
-# well known name set by `get-credentials`
+# well known name set by `gcloud container clusters get-credentials`
 gke_context="gke_${cluster_project}_${cluster_region}_${cluster_name}"
 context="${KUBECTL_CONTEXT:-${gke_context}}"
 
 # coordinates to locate the app on the target cluster
-app="slack-infra"
 namespace="${app}"
 
 # ensure we have a context to talk to the target cluster
