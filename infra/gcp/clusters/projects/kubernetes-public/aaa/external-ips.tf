7b35015fb6706c27f8053927cdc0f4c199548feb
Aaron Crickenberger <spiffxp@gmail.com>
2021-08-03 09:45:40 -0700

    clusters/aaa: terraform fmt

--- infra/gcp/clusters/projects/kubernetes-public/aaa/external-ips.tf
+++ infra/gcp/clusters/projects/kubernetes-public/aaa/external-ips.tf
@@ -4,24 +4,24 @@ This file defines all the external IP addresses in kubernetes-public
 
 // used by gcsweb.k8s.io
 resource "google_compute_global_address" "gcsweb_k8s_io" {
-  project       = data.google_project.project.project_id
-  name          = "gcsweb-k8s-io"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "gcsweb-k8s-io"
+  address_type = "EXTERNAL"
 }
 
 // used by canary.k8s.io
 resource "google_compute_global_address" "k8s_io_ingress_canary" {
-  project       = data.google_project.project.project_id
-  name          = "k8s-io-ingress-canary"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "k8s-io-ingress-canary"
+  address_type = "EXTERNAL"
 }
 
 // used by canary.k8s.io (IPv6)
 resource "google_compute_global_address" "k8s_io_ingress_canary_v6" {
-  project       = data.google_project.project.project_id
-  name          = "k8s-io-ingress-canary-v6"
-  address_type  = "EXTERNAL"
-  ip_version    = "IPV6"
+  project      = data.google_project.project.project_id
+  name         = "k8s-io-ingress-canary-v6"
+  address_type = "EXTERNAL"
+  ip_version   = "IPV6"
 }
 
 // used by k8s-infra-prow.k8s.io
@@ -41,45 +41,45 @@ resource "google_compute_global_address" "k8s_infra_prow_v6" {
 
 // used by k8s.io
 resource "google_compute_global_address" "k8s_io_ingress_prod" {
-  project       = data.google_project.project.project_id
-  name          = "k8s-io-ingress-prod"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "k8s-io-ingress-prod"
+  address_type = "EXTERNAL"
 }
 
 // used by k8s.io (IPv6)
 resource "google_compute_global_address" "k8s_io_ingress_prod_v6" {
-  project       = data.google_project.project.project_id
-  name          = "k8s-io-ingress-prod-v6"
-  address_type  = "EXTERNAL"
-  ip_version    = "IPV6"
+  project      = data.google_project.project.project_id
+  name         = "k8s-io-ingress-prod-v6"
+  address_type = "EXTERNAL"
+  ip_version   = "IPV6"
 }
 
 // used by perf-dash.k8s.io
 resource "google_compute_global_address" "perf_dash" {
-  project       = data.google_project.project.project_id
-  name          = "perf-dash-k8s-io-ingress-prod"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "perf-dash-k8s-io-ingress-prod"
+  address_type = "EXTERNAL"
 }
 
 // used by sippy.k8s.io
 resource "google_compute_global_address" "sippy" {
-  project       = data.google_project.project.project_id
-  name          = "sippy-ingress-prod"
-  description   = "IP for aaa cluster Ingress"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "sippy-ingress-prod"
+  description  = "IP for aaa cluster Ingress"
+  address_type = "EXTERNAL"
 }
 
 // used by slack.k8s.io
 resource "google_compute_global_address" "slack" {
-  project       = data.google_project.project.project_id
-  name          = "slack-infra-ingress-prod"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "slack-infra-ingress-prod"
+  address_type = "EXTERNAL"
 }
 
 // used by release.triage.k8s.io
 resource "google_compute_global_address" "release_triage" {
-  project       = data.google_project.project.project_id
-  name          = "triage-party-release-ingress-prod"
-  description   = "IP for aaa cluster Ingress"
-  address_type  = "EXTERNAL"
+  project      = data.google_project.project.project_id
+  name         = "triage-party-release-ingress-prod"
+  description  = "IP for aaa cluster Ingress"
+  address_type = "EXTERNAL"
 }
