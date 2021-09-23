dcae7437a789c16b5c9dd7ac59e38b53d452cac0
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-09-24 09:17:21 +1200

    Remove hardcoded reference of GCP_PROJECT

--- images/public-log-vendor-matcher/app/ext-ip-asn.sql
+++ images/public-log-vendor-matcher/app/ext-ip-asn.sql
@@ -5,5 +5,5 @@ SELECT
     ip_end as end_ip,
     NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip_start)) AS start_ip_int,
     NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip_end)) AS end_ip_int
-    FROM `k8s-infra-ii-sandbox.${GCP_BIGQUERY_DATASET_WITH_DATE}.pyasn_ip_asn_extended`
+    FROM `${GCP_PROJECT}.${GCP_BIGQUERY_DATASET_WITH_DATE}.pyasn_ip_asn_extended`
     WHERE regexp_contains(ip_start, r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}");
