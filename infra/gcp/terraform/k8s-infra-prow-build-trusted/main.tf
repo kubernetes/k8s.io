ad8d03744a189aff28baee8dff6437940e7d6464
Aaron Crickenberger <spiffxp@gmail.com>
2021-09-29 06:19:11 -0700

    tf/k8s-infra-prow-build-trusted: cleanup
    Specifically:
    
    - merge locals blocks
    - concert workload_identity_service_accounts to map
    - terraform fmt
    - fixup wisa module invocation
    - depend on module.project.project_id

--- infra/gcp/terraform/k8s-infra-prow-build-trusted/main.tf
+++ infra/gcp/terraform/k8s-infra-prow-build-trusted/main.tf
@@ -29,100 +29,33 @@ locals {
   bigquery_location = "US"                 // The bigquery specific location where the dataset should be created
   pod_namespace     = "test-pods"          // MUST match whatever prow is configured to use when it schedules to this cluster
 
-  workload_identity_service_accounts = [
-    {
-      name        = "prow-build-trusted"
+  workload_identity_service_accounts = {
+    prow-build-trusted = {
       description = "default service account for pods in ${local.cluster_name}"
-    },
-    {
+    }
+    gcb-builder = {
       name        = "gcb-builder"
       description = "trigger GCB builds in all k8s-staging projects"
-    },
-    {
-      name          = "prow-deployer"
+    }
+    prow-deployer = {
       description   = "deploys k8s resources to k8s clusters"
       project_roles = ["roles/container.admin"] // also some assigned in ensure-main-project
-    },
-    {
-      name          = "k8s-metrics"
+    }
+    k8s-metrics = {
       description   = "read bigquery and write to gs://k8s-metrics"
       project_roles = ["roles/bigquery.user"]
-    },
-    {
-      name          = "k8s-triage"
+    }
+    k8s-triage = {
       description   = "read bigquery and write to gs://k8s-triage"
       project_roles = ["roles/bigquery.user"]
-    },
-    {
-      name              = "kubernetes-external-secrets"
+    }
+    kubernetes-external-secrets = {
       description       = "sync K8s secrets from GSM in this and other projects"
       project_roles     = ["roles/secretmanager.secretAccessor"]
       cluster_namespace = "kubernetes-external-secrets"
     }
-  ]
-  // Service Accounts in ${pod_namespace} (usable via Workload Identity)
-  prow_deployer_sa_name = "prow-deployer" // Allowed to deploy to prow build clusters
-}
+  }
 
-data "google_organization" "org" {
-  domain = "kubernetes.io"
-}
-
-module "project" {
-  source       = "../modules/gke-project"
-  project_id   = local.project_id
-  project_name = local.project_id
-}
-
-// Ensure k8s-infra-prow-oncall@kuberentes.io has owner access to this project
-resource "google_project_iam_member" "k8s_infra_prow_oncall" {
-  project = local.project_id
-  role    = "roles/owner"
-  member  = "group:k8s-infra-prow-oncall@kubernetes.io"
-}
-
-// TODO: consider moving the project role binding resources into the
-//       workload-identity-service-account module
-// 
-// Some of the roles are assigned in bash or other terraform modules, so as
-// to keep the permissions necessary to run this terraform module scoped to
-// "roles/owner" for local.project_id
-
-module "workload_identity_service_accounts" {
-  for_each          = { for x in local.workload_identity_service_accounts : x.name => x }
-  source            = "../modules/workload-identity-service-account"
-  project_id        = local.project_id
-  name              = each.value.name
-  description       = each.value.description
-  cluster_namespace = lookup(each.value, "pod_namespace", local.pod_namespace)
-  project_roles     = lookup(each.value, "project_roles", [])
-}
-
-// TODO: this role belongs in k8s-infra-prow-build
-resource "google_project_iam_member" "prow_deployer_for_prow_build" {
-  project = "k8s-infra-prow-build"
-  role    = "roles/container.admin"
-  member  = "serviceAccount:prow-deployer@k8s-infra-prow-build-trusted.iam.gserviceaccount.com"
-}
-
-// external (regional) ip addresses
-resource "google_compute_address" "kubernetes_external_secrets_metrics_address" {
-  name         = "kubernetes-external-secrets-metrics"
-  description  = "to allow monitoring.k8s.prow.io to scrape kubernetes-external-secrets metrics"
-  project      = local.project_id
-  region       = local.cluster_location
-  address_type = "EXTERNAL"
-}
-
-resource "google_compute_address" "ghproxy_metrics_address" {
-  name         = "ghproxy-metrics"
-  description  = "to allow monitoring.k8s.prow.io to scrape ghproxy metrics"
-  project      = local.project_id
-  region       = local.cluster_location
-  address_type = "EXTERNAL"
-}
-
-locals {
   build_cluster_secrets = {
     k8s-infra-kops-e2e-tests-aws-ssh-key = {
       group  = "sig-cluster-lifecycle"
@@ -143,9 +76,64 @@ locals {
   }
 }
 
+data "google_organization" "org" {
+  domain = "kubernetes.io"
+}
+
+module "project" {
+  source       = "../modules/gke-project"
+  project_id   = local.project_id
+  project_name = local.project_id
+}
+
+// Ensure k8s-infra-prow-oncall@kuberentes.io has owner access to this project
+resource "google_project_iam_member" "k8s_infra_prow_oncall" {
+  project = module.project.project_id
+  role    = "roles/owner"
+  member  = "group:k8s-infra-prow-oncall@kubernetes.io"
+}
+
+// Some of the roles for these service accounts are assigned in bash or other
+// terraform modules, so as to keep the permissions necessary to run this
+// terraform module scoped to "roles/owner" for module.project.project_id
+
+module "workload_identity_service_accounts" {
+  for_each          = local.workload_identity_service_accounts
+  source            = "../modules/workload-identity-service-account"
+  project_id        = module.project.project_id
+  name              = each.key
+  description       = each.value.description
+  cluster_namespace = lookup(each.value, "cluster_namespace", local.pod_namespace)
+  project_roles     = lookup(each.value, "project_roles", [])
+}
+
+// TODO: this role belongs in k8s-infra-prow-build
+resource "google_project_iam_member" "prow_deployer_for_prow_build" {
+  project = "k8s-infra-prow-build"
+  role    = "roles/container.admin"
+  member  = "serviceAccount:prow-deployer@k8s-infra-prow-build-trusted.iam.gserviceaccount.com"
+}
+
+// external (regional) ip addresses
+resource "google_compute_address" "kubernetes_external_secrets_metrics_address" {
+  name         = "kubernetes-external-secrets-metrics"
+  description  = "to allow monitoring.k8s.prow.io to scrape kubernetes-external-secrets metrics"
+  project      = module.project.project_id
+  region       = local.cluster_location
+  address_type = "EXTERNAL"
+}
+
+resource "google_compute_address" "ghproxy_metrics_address" {
+  name         = "ghproxy-metrics"
+  description  = "to allow monitoring.k8s.prow.io to scrape ghproxy metrics"
+  project      = module.project.project_id
+  region       = local.cluster_location
+  address_type = "EXTERNAL"
+}
+
 resource "google_secret_manager_secret" "build_cluster_secrets" {
   for_each  = local.build_cluster_secrets
-  project   = local.project_id
+  project   = module.project.project_id
   secret_id = each.key
   labels = {
     group = each.value.group
@@ -168,7 +156,7 @@ resource "google_secret_manager_secret_iam_binding" "build_cluster_secret_admins
 
 module "prow_build_cluster" {
   source             = "../modules/gke-cluster"
-  project_name       = local.project_id
+  project_name       = module.project.project_id
   cluster_name       = local.cluster_name
   cluster_location   = local.cluster_location
   bigquery_location  = local.bigquery_location
@@ -180,7 +168,7 @@ module "prow_build_cluster" {
 
 module "prow_build_nodepool" {
   source        = "../modules/gke-nodepool"
-  project_name  = local.project_id
+  project_name  = module.project.project_id
   cluster_name  = module.prow_build_cluster.cluster.name
   location      = module.prow_build_cluster.cluster.location
   name          = "trusted-pool1"
