ea710a2cd594927f58b3f7d290df848069c3ec0b
Aaron Crickenberger <spiffxp@gmail.com>
2021-09-29 06:34:56 -0700

    tf/k8s-infra-prow-build: split up main.tf
    Move resources out of main.tf into their own files to reduce the set of
    resources someone needs to look at all at once to make a change
    
    - secrets.tf
    - serviceaccounts.tf
    - external-ips.tf

--- infra/gcp/terraform/k8s-infra-prow-build/main.tf
+++ infra/gcp/terraform/k8s-infra-prow-build/main.tf
@@ -16,11 +16,11 @@ limitations under the License.
 
 /*
 This file defines:
+- Shared local values for use by other files in this module
 - GCP Project k8s-infra-prow-build to hold a prow build cluster
-- GCP Service Account for k8s-infra-prow-build pods (bound via workload identity to a KSA of the same name)
-- GCP Service Account for boskos-janitor (bound via workload identity to a KSA of the same name)
-- GKE cluster configuration for prow-build
-- GKE nodepool configuration for prow-build
+- Project-level IAM bindings
+- GKE cluster configuration for the build cluster
+- GKE nodepool configuration for the build cluster
 */
 
 locals {
@@ -29,31 +29,6 @@ locals {
   cluster_location  = "us-central1" // The GCP location (region or zone) where the cluster should be created
   bigquery_location = "US"          // The bigquery specific location where the dataset should be created
   pod_namespace     = "test-pods"   // MUST match whatever prow is configured to use when it schedules to this cluster
-
-  workload_identity_service_accounts = {
-    prow-build = {
-      description = "default service account for pods in ${local.cluster_name}"
-    }
-    boskos-janitor = {
-      description = "used by boskos-janitor in ${local.cluster_name}"
-    }
-    kubernetes-external-secrets = {
-      description       = "sync K8s secrets from GSM in this and other projects"
-      project_roles     = ["roles/secretmanager.secretAccessor"],
-      cluster_namespace = "kubernetes-external-secrets"
-    }
-  }
-
-  build_cluster_secrets = {
-    prow-build-service-account = {
-      group  = "sig-testing"
-      owners = "k8s-infra-prow-oncall@kubernetes.io"
-    }
-    prow-build-ssh-key-secret = {
-      group  = "sig-testing"
-      owners = "k8s-infra-prow-oncall@kubernetes.io"
-    }
-  }
 }
 
 data "google_organization" "org" {
@@ -91,65 +66,6 @@ resource "google_project_iam_member" "prow_deployer_for_prow_build" {
   project = module.project.project_id
   role    = "roles/container.admin"
   member  = "serviceAccount:prow-deployer@k8s-infra-prow-build-trusted.iam.gserviceaccount.com"
-}
-
-module "workload_identity_service_accounts" {
-  for_each          = local.workload_identity_service_accounts
-  source            = "../modules/workload-identity-service-account"
-  project_id        = module.project.project_id
-  name              = each.key
-  description       = each.value.description
-  cluster_namespace = lookup(each.value, "cluster_namespace", local.pod_namespace)
-  project_roles     = lookup(each.value, "project_roles", [])
-}
-
-// external ip formerly managed by infra/gcp/bash/prow/ensure-e2e-projects.sh
-resource "google_compute_address" "boskos_metrics" {
-  name         = "boskos-metrics"
-  description  = "to allow monitoring.k8s.prow.io to scrape boskos metrics"
-  project      = module.project.project_id
-  region       = local.cluster_location
-  address_type = "EXTERNAL"
-}
-
-// external ip formerly managed by infra/gcp/bash/prow/ensure-e2e-projects.sh
-resource "google_compute_address" "greenhouse_metrics" {
-  name         = "greenhouse-metrics"
-  description  = "to allow monitoring.k8s.prow.io to scrape greenhouse metrics"
-  project      = module.project.project_id
-  region       = local.cluster_location
-  address_type = "EXTERNAL"
-}
-
-resource "google_compute_address" "kubernetes_external_secrets_metrics" {
-  name         = "kubernetes-external-secrets-metrics"
-  description  = "to allow monitoring.k8s.prow.io to scrape kubernetes-external-secrets metrics"
-  project      = module.project.project_id
-  region       = local.cluster_location
-  address_type = "EXTERNAL"
-}
-
-resource "google_secret_manager_secret" "build_cluster_secrets" {
-  for_each  = local.build_cluster_secrets
-  project   = module.project.project_id
-  secret_id = each.key
-  labels = {
-    group = each.value.group
-  }
-  replication {
-    automatic = true
-  }
-}
-
-resource "google_secret_manager_secret_iam_binding" "build_cluster_secret_admins" {
-  for_each  = local.build_cluster_secrets
-  project   = google_secret_manager_secret.build_cluster_secrets[each.key].project
-  secret_id = google_secret_manager_secret.build_cluster_secrets[each.key].id
-  role      = "roles/secretmanager.admin"
-  members = [
-    "group:k8s-infra-prow-oncall@kubernetes.io",
-    "group:${each.value.owners}"
-  ]
 }
 
 module "prow_build_cluster" {
---
+++ infra/gcp/terraform/k8s-infra-prow-build/serviceaccounts.tf
@@ -0,0 +1,41 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+locals {
+  workload_identity_service_accounts = {
+    prow-build = {
+      description = "default service account for pods in ${local.cluster_name}"
+    }
+    boskos-janitor = {
+      description = "used by boskos-janitor in ${local.cluster_name}"
+    }
+    kubernetes-external-secrets = {
+      description       = "sync K8s secrets from GSM in this and other projects"
+      project_roles     = ["roles/secretmanager.secretAccessor"],
+      cluster_namespace = "kubernetes-external-secrets"
+    }
+  }
+}
+
+module "workload_identity_service_accounts" {
+  for_each          = local.workload_identity_service_accounts
+  source            = "../modules/workload-identity-service-account"
+  project_id        = module.project.project_id
+  name              = each.key
+  description       = each.value.description
+  cluster_namespace = lookup(each.value, "cluster_namespace", local.pod_namespace)
+  project_roles     = lookup(each.value, "project_roles", [])
+}
