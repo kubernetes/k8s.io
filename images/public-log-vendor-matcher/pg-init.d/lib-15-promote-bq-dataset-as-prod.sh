93b336b30a778788325f9c0c6be32daf9228a2be
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-10-01 11:40:26 +1300

    Fix bash issue with undeclared variable

--- images/public-log-vendor-matcher/pg-init.d/lib-15-promote-bq-dataset-as-prod.sh
+++ images/public-log-vendor-matcher/pg-init.d/lib-15-promote-bq-dataset-as-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Purpose: Promote all the tables to the un-timestamped dataset
 
-if [ ! "${NO_PROMOTE}" = "true" ]; then
+if [ ! "${NO_PROMOTE:-}" = "true" ]; then
   for TABLE in $(bq ls "${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}" | awk '{print $1}' | tail +3 | xargs); do
     echo "Removing table '${GCP_BIGQUERY_DATASET}.$TABLE'"
     bq rm -f "${GCP_BIGQUERY_DATASET}.$TABLE" > "${BQ_OUTPUT:-/dev/null}" 2>&1
