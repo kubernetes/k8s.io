2954c04b5665eef02fbbd366f03fa9cbfa455bb8
Madhav Jivrajani <madhav.jiv@gmail.com>
2021-10-04 13:34:59 +0530

    add fake clients for admin and group services
    Signed-off-by: Madhav Jivrajani <madhav.jiv@gmail.com>

---
+++ groups/fake/fake_client.go
@@ -0,0 +1,206 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+import (
+	"fmt"
+
+	admin "google.golang.org/api/admin/directory/v1"
+	groupssettings "google.golang.org/api/groupssettings/v1"
+)
+
+// FakeAdminServiceClient implements the AdminServiceClient but is fake.
+type FakeAdminServiceClient struct {
+	// Groups is a mapping from groupKey to *admin.Group
+	Groups map[string]*admin.Group
+	// Members is a mapping from groupKey -> members of that group
+	// Members of a group are a mapping from memberKey -> *admin.Member
+	Members map[string]map[string]*admin.Member
+	// OnGroupInsert is a callback function called whenever an
+	// Insert operation is done for a group. This is nescessary
+	// because creating a group also involves reflecting that
+	// creation in the group service fake client, where the GsGroups
+	// map takes the group email as the key.
+	onGroupInsert func(string)
+}
+
+func NewFakeAdminServiceClient() *FakeAdminServiceClient {
+	return &FakeAdminServiceClient{
+		Groups:  make(map[string]*admin.Group),
+		Members: make(map[string]map[string]*admin.Member),
+	}
+}
+
+func (fasc *FakeAdminServiceClient) WithCallback(onGroupInsert func(string)) *FakeAdminServiceClient {
+	fasc.onGroupInsert = onGroupInsert
+	return fasc
+}
+
+func (fasc *FakeAdminServiceClient) GetGroup(groupKey string) (*admin.Group, error) {
+	group, ok := fasc.Groups[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("group key %s not found", groupKey)
+	}
+
+	return group, nil
+}
+
+func (fasc *FakeAdminServiceClient) GetMember(groupKey, memberKey string) (*admin.Member, error) {
+	members, ok := fasc.Members[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("group with group key %s not found", groupKey)
+	}
+
+	member, ok := members[memberKey]
+	if !ok {
+		return nil, fmt.Errorf("member with groupKey %s and memberKey %s not found", groupKey, memberKey)
+	}
+
+	return member, nil
+}
+
+func (fasc *FakeAdminServiceClient) ListGroups() (*admin.Groups, error) {
+	return fasc.constructAdminGroups(), nil
+}
+
+func (fasc *FakeAdminServiceClient) ListMembers(groupKey string) (*admin.Members, error) {
+	_, ok := fasc.Members[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("groupKey %s not found", groupKey)
+	}
+
+	return fasc.constructAdminMembers(groupKey), nil
+}
+
+func (fasc *FakeAdminServiceClient) InsertGroup(group *admin.Group) (*admin.Group, error) {
+	fasc.Groups[group.Email] = group
+	if fasc.onGroupInsert != nil {
+		fasc.onGroupInsert(group.Email)
+	}
+
+	fasc.Members[group.Email] = map[string]*admin.Member{}
+	return group, nil
+}
+
+func (fasc *FakeAdminServiceClient) InsertMember(groupKey string, member *admin.Member) (*admin.Member, error) {
+	_, ok := fasc.Members[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("groupKey %s not found", groupKey)
+	}
+
+	fasc.Members[groupKey][member.Email] = member
+	return member, nil
+}
+
+func (fasc *FakeAdminServiceClient) UpdateGroup(groupKey string, group *admin.Group) (*admin.Group, error) {
+	_, ok := fasc.Groups[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("group key %s not found", groupKey)
+	}
+
+	fasc.Groups[groupKey] = group
+	return group, nil
+}
+
+func (fasc *FakeAdminServiceClient) UpdateMember(groupKey, memberKey string, member *admin.Member) (*admin.Member, error) {
+	_, ok := fasc.Members[groupKey]
+	if !ok {
+		return nil, fmt.Errorf("group with group key %s not found", groupKey)
+	}
+
+	_, ok = fasc.Members[groupKey][memberKey]
+	if !ok {
+		return nil, fmt.Errorf("member with groupKey %s and memberKey %s not found", groupKey, memberKey)
+	}
+
+	fasc.Members[groupKey][memberKey] = member
+	return member, nil
+}
+
+func (fasc *FakeAdminServiceClient) DeleteGroup(groupKey string) error {
+	_, ok := fasc.Groups[groupKey]
+	if !ok {
+		return fmt.Errorf("group key %s not found", groupKey)
+	}
+
+	delete(fasc.Groups, groupKey)
+	return nil
+}
+
+func (fasc *FakeAdminServiceClient) DeleteMember(groupKey, memberKey string) error {
+	_, ok := fasc.Members[groupKey]
+	if !ok {
+		return fmt.Errorf("group with group key %s not found", groupKey)
+	}
+
+	_, ok = fasc.Members[groupKey][memberKey]
+	if !ok {
+		return fmt.Errorf("member with groupKey %s and memberKey %s not found", groupKey, memberKey)
+	}
+
+	delete(fasc.Members[groupKey], memberKey)
+	return nil
+}
+
+func (fasc *FakeAdminServiceClient) constructAdminGroups() *admin.Groups {
+	groups := &admin.Groups{}
+	for _, group := range fasc.Groups {
+		groups.Groups = append(groups.Groups, group)
+	}
+
+	return groups
+}
+
+// This function assumes that groupKey is present in the members map.
+func (fasc *FakeAdminServiceClient) constructAdminMembers(groupKey string) *admin.Members {
+	members := &admin.Members{}
+	for _, member := range fasc.Members[groupKey] {
+		members.Members = append(members.Members, member)
+	}
+
+	return members
+}
+
+// FakeGroupServiceClient implements the GroupServiceClient but is fake.
+type FakeGroupServiceClient struct {
+	GsGroups map[string]*groupssettings.Groups
+}
+
+func NewFakeGroupServiceClient() *FakeGroupServiceClient {
+	return &FakeGroupServiceClient{
+		GsGroups: make(map[string]*groupssettings.Groups),
+	}
+}
+
+func (fgsc *FakeGroupServiceClient) Get(groupUniqueID string) (*groupssettings.Groups, error) {
+	gsg, ok := fgsc.GsGroups[groupUniqueID]
+	if !ok {
+		return nil, fmt.Errorf("groupUniqueID not found %ss", groupUniqueID)
+	}
+
+	return gsg, nil
+}
+
+func (fgsc *FakeGroupServiceClient) Patch(groupUniqueID string, groups *groupssettings.Groups) (*groupssettings.Groups, error) {
+	_, ok := fgsc.GsGroups[groupUniqueID]
+	if !ok {
+		return nil, fmt.Errorf("groupUniqueID not found %ss", groupUniqueID)
+	}
+
+	fgsc.GsGroups[groupUniqueID] = groups
+	return groups, nil
+}
