3d4fe9a599e378fe1ba8518f0517b5ca4126b181
Aaron Crickenberger <spiffxp@gmail.com>
2021-10-14 12:00:24 -0700

    tf/k8s-infra-kubernetes-io: add k8s-infra-monthly budget
    $3M/y / 12 mo/y = $250K/mo, alert at 90% (225K) and 100%

--- infra/gcp/terraform/k8s-infra-kubernetes-io/main.tf
+++ infra/gcp/terraform/k8s-infra-kubernetes-io/main.tf
@@ -65,6 +65,7 @@ resource "google_billing_budget" "capg_budget" {
   billing_account = data.google_billing_account.account.billing_account
   display_name = "k8s-infra-sandbox-capg"
   budget_filter {
+    # calendar_period = "MONTH" # TODO: terraform doesn't support this?
     projects = [ "projects/${data.google_project.k8s_infra_sandbox_capg.number}" ]
     # exclude promotions, which is where our credits come from, since that zeros out cost
     credit_types_treatment = "INCLUDE_SPECIFIED_CREDITS"
@@ -93,6 +94,44 @@ resource "google_billing_budget" "capg_budget" {
   }
   dynamic "threshold_rules" {
     for_each = toset([0.2, 0.5, 0.9, 1.0])
+    content {
+      threshold_percent = threshold_rules.value
+    }
+  }
+}
+
+resource "google_billing_budget" "k8s_infra" {
+  billing_account = data.google_billing_account.account.billing_account
+  display_name = "k8s-infra-monthly"
+  budget_filter {
+    # calendar_period = "MONTH" # TODO: terraform doesn't support this?
+    # exclude promotions, which is where our credits come from, since that zeros out cost
+    credit_types_treatment = "INCLUDE_SPECIFIED_CREDITS"
+    credit_types           = [
+        "SUSTAINED_USAGE_DISCOUNT",
+        "DISCOUNT",
+        "COMMITTED_USAGE_DISCOUNT",
+        "FREE_TIER",
+        "COMMITTED_USAGE_DISCOUNT_DOLLAR_BASE",
+        "SUBSCRIPTION_BENEFIT",
+    ]
+  }
+  amount {
+    specified_amount {
+      currency_code = "USD"
+      units = "250000" # 3M/yr / 12mo
+    }
+  }
+  all_updates_rule {
+    # Don't send to users with Billing Account Administrators and
+    # Billing Account Users IAM roles for the billing account
+    disable_default_iam_recipients = true
+    monitoring_notification_channels = [
+      data.google_monitoring_notification_channel.sig_k8s_infra_leads.name
+    ]
+  }
+  dynamic "threshold_rules" {
+    for_each = toset([0.9, 1.0])
     content {
       threshold_percent = threshold_rules.value
     }
