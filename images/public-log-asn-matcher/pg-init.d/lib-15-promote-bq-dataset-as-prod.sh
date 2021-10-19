dd29d02efc1564275354237e38b6dc92251cb307
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-10-19 15:47:20 +1300

    Update public-log-asn-matcher table promotion
    Only promote the 7_asn_company_c_ip_lookup table

--- images/public-log-asn-matcher/pg-init.d/lib-15-promote-bq-dataset-as-prod.sh
+++ images/public-log-asn-matcher/pg-init.d/lib-15-promote-bq-dataset-as-prod.sh
@@ -16,12 +16,11 @@
 # Purpose: Promote all the tables to the un-timestamped dataset
 
 if [ ! "${NO_PROMOTE:-}" = "true" ]; then
-  for TABLE in $(bq ls "${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}" | awk '{print $1}' | tail +3 | xargs); do
+    TABLE="7_asn_company_c_ip_lookup"
     echo "Removing table '${GCP_BIGQUERY_DATASET}.$TABLE'"
     bq rm -f "${GCP_BIGQUERY_DATASET}.$TABLE" > "${BQ_OUTPUT:-/dev/null}" 2>&1
     echo "Copying table '${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}.$TABLE' to '${GCP_BIGQUERY_DATASET}.$TABLE'"
     bq cp --noappend_table --nono_clobber -f "${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}.$TABLE" "${GCP_BIGQUERY_DATASET}.$TABLE" >> "${BQ_OUTPUT:-/dev/null}" 2>&1
-  done
 else
     echo "Promotion of dataset tables disabled."
 fi
