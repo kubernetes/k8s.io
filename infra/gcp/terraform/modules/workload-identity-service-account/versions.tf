30f38d951036827842b5f51ca43d99f24ef16b24
Arnaud Meukam <ameukam@gmail.com>
2021-10-29 00:58:29 +0200

    infra/gcp: Bump terraform provider
    Changelog: https://github.com/hashicorp/terraform-provider-google-beta/blob/master/CHANGELOG.md#3890-october-18-2021
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/modules/workload-identity-service-account/versions.tf
+++ infra/gcp/terraform/modules/workload-identity-service-account/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.87.0"
+      version = "~> 3.89.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.87.0"
+      version = "~> 3.89.0"
     }
   }
 }
