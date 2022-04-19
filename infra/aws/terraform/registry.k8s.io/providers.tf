8d755ffc74d6f8d9fd9c2d2ad4d175fd6619e971
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-04-19 14:44:27 +1200

    Add production serving regions for registry.k8s.io

--- infra/aws/terraform/registry.k8s.io/providers.tf
+++ infra/aws/terraform/registry.k8s.io/providers.tf
@@ -36,3 +36,33 @@ provider "aws" {
   alias  = "us-west-2"
   region = "us-west-2"
 }
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
+
+provider "aws" {
+  alias  = "ap-northeast-1"
+  region = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias  = "ap-south-1"
+  region = "ap-south-1"
+}
