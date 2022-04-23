8219f980d204345cc624987ba6022780fefb4db5
Arnaud Meukam <ameukam@gmail.com>
2022-04-23 10:22:58 +0200

    oci-proxy: disable CDN for the sandbox environment
    The CDN service is not needed at the moment to serve docker client
    requests.
    Also:
    - ignore changes done on the Cloud Run service.
    - Bumping the Google provider to a recent version
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/k8s-infra-oci-proxy/provider.tf
+++ infra/gcp/terraform/k8s-infra-oci-proxy/provider.tf
@@ -29,11 +29,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.9.0"
+      version = "~> 4.30.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.9.0"
+      version = "~> 4.30.0"
     }
   }
 }
