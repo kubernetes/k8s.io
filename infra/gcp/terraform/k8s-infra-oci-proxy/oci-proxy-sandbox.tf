08d1a5649c78baf7b96714debff22874d3842c55
Arnaud Meukam <ameukam@gmail.com>
2022-08-30 22:18:35 +0200

    Fix argument of the container image.
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/k8s-infra-oci-proxy/oci-proxy-sandbox.tf
+++ infra/gcp/terraform/k8s-infra-oci-proxy/oci-proxy-sandbox.tf
@@ -131,7 +131,7 @@ resource "google_cloud_run_service" "regions" {
       service_account_name = google_service_account.oci-proxy.email
       containers {
         image = local.image
-        args = [ "-v3" ]
+        args = [ "-v=3" ]
       }
       container_concurrency = 5
       // 30 seconds less than cloud scheduler maximum.
