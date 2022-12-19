ff77dc86fc1d9c964e42589c485cc246c95591cf
Justin SB <justinsb@google.com>
2022-12-19 16:55:01 +0000

    infra/aws: create S3 bucket for dev of artifacts.k8s.io
    As suggested in #4460, we create one bucket in the development account.
    
    We create an isolated configuration, the goal is to enable development,
    rather than testing the TF configuration.
    
    Verifying the TF configuration is done in #4460.

---
+++ infra/aws/terraform/test-artifacts.k8s.io/providers.tf
@@ -0,0 +1,49 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+terraform {
+  backend "s3" {
+    bucket = "test-artifacts-k8s-io-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+# Provider for AWS non-region-specific operations.
+provider "aws" {
+  # We still have to specify a region to use.
+  region = "us-east-2"
+
+  profile = "default"
+}
+
+# Provider for AWS us-east-2 operations.
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+
+  profile = "default"
+}
---
+++ infra/aws/terraform/test-artifacts.k8s.io/s3/variables.tf
@@ -0,0 +1,32 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+variable "prefix" {
+  type    = string
+  default = ""
+}
+
+variable "s3_replication_iam_role_arn" {
+  description = "IAM role assumed by S3 service for replication"
+  type        = string
+  default     = ""
+}
+
+variable "s3_replication_rules" {
+  description = "List of maps for S3 replication rules"
+  type        = list(map(string))
+  default     = []
+}
