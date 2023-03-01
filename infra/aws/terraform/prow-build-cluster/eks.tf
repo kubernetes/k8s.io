fae0a7eda405a007e05cf1366e820cb8603816d9
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-03-01 10:38:47 +0100

    Use IPv6 for prow-build-cluster
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- infra/aws/terraform/prow-build-cluster/eks.tf
+++ infra/aws/terraform/prow-build-cluster/eks.tf
@@ -30,9 +30,18 @@ module "eks" {
   # Manage aws-auth ConfigMap.
   manage_aws_auth_configmap = true
 
-  # We do not really care if we use IPv4 or IPv6 as we don't depend much on
-  # pod-to-pod networking.
-  cluster_ip_family = "ipv4"
+  # We use IPv6 because we require a large number of nodes and pods.
+  # With IPv4, we can use only /16 VPC, which is a blocker for running
+  # 100+ nodes.
+  cluster_ip_family = "ipv6"
+
+  # We are using the IRSA created below for permissions
+  # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
+  # and then turn this off after the cluster/node group is created. Without this initial policy,
+  # the VPC CNI fails to assign IPs and nodes cannot join the cluster
+  # See https://github.com/aws/containers-roadmap/issues/1666 for more context
+  # TODO - remove this policy once AWS releases a managed version similar to AmazonEKS_CNI_Policy (IPv4)
+  create_cni_ipv6_iam_policy = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
--- infra/aws/terraform/prow-build-cluster/irsa.tf
+++ infra/aws/terraform/prow-build-cluster/irsa.tf
@@ -25,7 +25,7 @@ module "vpc_cni_irsa" {
 
   role_name_prefix      = "VPC-CNI-IRSA"
   attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv4   = true
+  vpc_cni_enable_ipv4   = false
   vpc_cni_enable_ipv6   = true
 
   oidc_providers = {
