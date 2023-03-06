765b882ced10f0b8067943744ccfb9f2e5c33cdd
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-03-06 13:12:02 +0100

    prow-build-cluster: deploy AWS Load Balancer Controller
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- infra/aws/terraform/prow-build-cluster/irsa.tf
+++ infra/aws/terraform/prow-build-cluster/irsa.tf
@@ -56,6 +56,24 @@ module "ebs_csi_irsa" {
   tags = local.tags
 }
 
+# IAM policy used for AWS Load Balancer Controller.
+module "aws_load_balancer_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.11"
+
+  role_name_prefix                       = "LBCONTROLLER-IRSA"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+
+  tags = local.tags
+}
+
 # IAM policy used for Cluster Autoscaler.
 module "cluster_autoscaler_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
--- infra/aws/terraform/prow-build-cluster/kubernetes.tf
+++ infra/aws/terraform/prow-build-cluster/kubernetes.tf
@@ -40,6 +40,39 @@ module "metrics_server" {
   ]
 }
 
+# AWS Load Balancer Controller (ALB/NLB integration).
+resource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.4.8"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.aws_load_balancer_controller_irsa.iam_role_arn
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
+
 # AWS Secrets Manager integration
 resource "helm_release" "secrets_store_csi_driver" {
   name       = "secrets-store-csi-driver"
