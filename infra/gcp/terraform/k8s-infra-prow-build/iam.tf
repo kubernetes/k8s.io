209e3c91f022154e65906a33e2b622d17ec690a8
Arnaud Meukam <ameukam@gmail.com>
2023-03-15 14:47:51 +0100

    Fix workload identity provider
    Follow-up of:
      - https://github.com/kubernetes/k8s.io/pull/4930
    
     - Fix name of the workload identity provider pool.
     - Fix a required version of the terraform provider
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

---
+++ infra/gcp/terraform/k8s-infra-prow-build/iam.tf
@@ -0,0 +1,40 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+resource "google_iam_workload_identity_pool" "eks_cluster" {
+  project = module.project.project_id
+
+  workload_identity_pool_id = "prow-eks"
+  display_name              = "EKS Prow Cluster"
+  description = "Identity pool for CI on AWS using EKS clusters"
+}
+
+resource "google_iam_workload_identity_pool_provider" "eks_cluster" {
+  project = module.project.project_id
+
+  display_name = "AWS OIDC provider"
+  description = "Identity pool for CI on AWS using EKS clusters"
+  workload_identity_pool_id          = google_iam_workload_identity_pool.eks_cluster.workload_identity_pool_id
+  workload_identity_pool_provider_id = "oidc"
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+  oidc {
+    # From EKS cluster created in https://github.com/kubernetes/k8s.io/tree/main/infra/aws/terraform/prow-build-cluster
+    issuer_uri        = "https://oidc.eks.us-east-2.amazonaws.com/id/F8B73554FE6FBAF9B19569183FB39762"
+    allowed_audiences = ["https://sts.googleapis.com"]
+  }
+}
