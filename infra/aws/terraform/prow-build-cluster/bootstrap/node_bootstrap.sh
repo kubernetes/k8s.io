6f1412975305873d1b8ec98f364dbbad95235555
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-03-28 18:25:01 +0200

    eks-prow-build-cluster: use instance SSDs for container and kubelet
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

---
+++ infra/aws/terraform/prow-build-cluster/bootstrap/node_bootstrap.sh
@@ -0,0 +1,122 @@
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## intended to be used as a node pre-bootstrap script
+## based on: https://github.com/awslabs/amazon-eks-ami/pull/1171
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+## sysctl settings (required by Prow)
+sysctl -w fs.inotify.max_user_watches=524288
+
+## Set up ephemeral disks (SSDs) to be used by containerd and kubelet
+
+MNT_DIR="/mnt/k8s-disks"
+disks=($(find -L /dev/disk/by-id/ -xtype l -name '*NVMe_Instance_Storage_*'))
+
+if [[ "${#disks[@]}" -eq 0 ]]; then
+  echo "no ephemeral disks found, skipping disk setup"
+  exit 0
+fi
+
+# Get devices of NVMe instance storage ephemeral disks
+EPHEMERAL_DISKS=($(realpath "${disks[@]}" | sort -u))
+
+# Pick the first NVMe disk. In this case, we care about only one disk,
+# additional disks are not much of use for us.
+# We don't want to deal with RAID because we don't gain much from it.
+dev="${EPHEMERAL_DISKS[0]}"
+
+# Mounts and creates xfs file systems on chosen EC2 instance store NVMe disk
+# without existing file system. Mounts in /mnt/k8s-disks
+if [[ -z "$(lsblk "${dev}" -o fstype --noheadings)" ]]; then
+  mkfs.xfs -l su=8b "${dev}"
+fi
+
+if [[ ! -z "$(lsblk "${dev}" -o MOUNTPOINT --noheadings)" ]]; then
+  echo "${dev} is already mounted."
+  exit 0
+fi
+
+# Get mount point for the disk.
+mount_point="${MNT_DIR}"
+mount_unit_name="$(systemd-escape --path --suffix=mount "${mount_point}")"
+
+mkdir -p "${mount_point}"
+
+# Create systemd service to mount the disk.
+cat > "/etc/systemd/system/${mount_unit_name}" << EOF
+[Unit]
+Description=Mount EC2 Instance Store NVMe disk
+[Mount]
+What=${dev}
+Where=${mount_point}
+Type=xfs
+Options=defaults,noatime
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemd-analyze verify "${mount_unit_name}"
+systemctl enable "${mount_unit_name}" --now
+
+## Create mount points on SSD for containerd and kubelet
+prev_running=""
+needs_linked=""
+
+# Stop containerd and kubelet if they are running.
+for unit in "containerd" "kubelet"; do
+  if [[ "$(systemctl is-active var-lib-${unit}.mount)" != "active" ]]; then
+    if systemctl is-active "${unit}" > /dev/null 2>&1; then
+      prev_running+=" ${unit}"
+    fi
+    needs_linked+=" ${unit}"
+  fi
+done
+
+if [[ ! -z "${prev_running}" ]]; then
+  systemctl stop ${prev_running}
+fi
+
+# Transfer state directories to the disk, if they exist.
+for unit in ${needs_linked}; do
+  var_lib_mount_point="/var/lib/${unit}"
+  unit_mount_point="${mount_point}/${unit}"
+
+  echo "Copying ${var_lib_mount_point}/ to ${unit_mount_point}/"
+  cp -a "${var_lib_mount_point}/" "${unit_mount_point}/"
+  
+  mount_unit_name="$(systemd-escape --path --suffix=mount "${var_lib_mount_point}")"
+  
+  cat > "/etc/systemd/system/${mount_unit_name}" << EOF
+    [Unit]
+    Description=Mount ${unit} on EC2 Instance Store NVMe disk
+    [Mount]
+    What=${unit_mount_point}
+    Where=${var_lib_mount_point}
+    Type=none
+    Options=bind
+    [Install]
+    WantedBy=multi-user.target
+EOF
+  systemd-analyze verify "${mount_unit_name}"
+  systemctl enable "${mount_unit_name}" --now
+done
+
+# Start again stopped services.
+if [[ ! -z "${prev_running}" ]]; then
+  systemctl start ${prev_running}
+fi
