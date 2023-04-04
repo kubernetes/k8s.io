94f7a1a2a1371d8954ad016dd915bb31c67b4b3f
Terry Howe <tlhowe@amazon.com>
2023-04-04 11:47:51 -0600

    Fix issues for new version of shellcheck

--- images/public-log-asn-matcher/pg-init.d/lib-13-add-c-ip-int-to-usage-all.sh
+++ images/public-log-asn-matcher/pg-init.d/lib-13-add-c-ip-int-to-usage-all.sh
@@ -20,4 +20,4 @@ export GCP_BIGQUERY_DATASET_WITH_DATE
 if [ -n "${GCP_BIGQUERY_DATASET_LOGS:-}" ]; then
     USE_LOGS="_no_logs"
 fi
-envsubst < /app/add_c_ip_int_to_usage_all${USE_LOGS:-}.sql | bq query --nouse_legacy_sql --replace --destination_table "${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}.usage_all_raw_int" >> "${BQ_OUTPUT:-/dev/null}" 2>&1
+envsubst < "/app/add_c_ip_int_to_usage_all${USE_LOGS:-}.sql" | bq query --nouse_legacy_sql --replace --destination_table "${GCP_BIGQUERY_DATASET}_${PIPELINE_DATE}.usage_all_raw_int" >> "${BQ_OUTPUT:-/dev/null}" 2>&1
