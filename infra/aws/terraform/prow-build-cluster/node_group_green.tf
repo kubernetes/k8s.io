3498964c5f407e0e58b64160093f4b1e8989176b
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-09-13 11:43:08 +0200

    eks-prow-build: switch green node group to public subnet
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- infra/aws/terraform/prow-build-cluster/node_group_green.tf
+++ infra/aws/terraform/prow-build-cluster/node_group_green.tf
@@ -23,7 +23,7 @@ locals {
     taints = var.node_taints_green
     labels = var.node_labels_green
 
-    subnet_ids = module.vpc.private_subnets
+    subnet_ids = module.vpc.public_subnets
 
     min_size     = var.node_min_size_green
     max_size     = var.node_max_size_green
