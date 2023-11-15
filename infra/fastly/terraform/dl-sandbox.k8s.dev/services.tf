8b7421916dba4c7442be7ea382895f597c119d35
Arnaud Meukam <ameukam@gmail.com>
2023-11-15 15:58:31 +0100

    Fastly: Create a service for cdn.dl-sandbox.k8s.dev
    Set up a new VCL service to investigate VCL service configuration using
    a private gcs bucket as an origin.
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

---
+++ infra/fastly/terraform/dl-sandbox.k8s.dev/services.tf
@@ -0,0 +1,172 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+locals {
+  domain_name = "cdn.dl-sandbox.k8s.dev"
+}
+
+resource "fastly_service_vcl" "files" {
+  name     = local.domain_name
+  activate = true
+
+  domain {
+    name = local.domain_name
+  }
+
+  backend {
+    name             = "GCS"
+    auto_loadbalance = false
+
+    healthcheck = "GCS Health"
+
+    address           = "storage.googleapis.com"
+    port              = 443
+    use_ssl           = true
+    ssl_cert_hostname = "storage.googleapis.com"
+    ssl_sni_hostname  = "storage.googleapis.com"
+
+    override_host = "${var.bucket}.storage.googleapis.com"
+
+    connect_timeout       = 5000  # milliseconds
+    between_bytes_timeout = 15000 # milliseconds
+    error_threshold       = 5
+  }
+
+  healthcheck {
+    name = "GCS Health"
+
+    host           = "${var.bucket}.storage.googleapis.com"
+    method         = "GET"
+    path           = "/"
+    check_interval = 3000
+    timeout        = 2000
+    threshold      = 2
+    initial        = 2
+    window         = 4
+  }
+
+  snippet {
+    content  = <<-EOT
+      if (req.url.path ~ "^/release/") {
+        set req.enable_segmented_caching = true;
+      }
+    EOT
+    name     = "Enable segment caching for large files"
+    priority = 60
+    type     = "recv"
+  }
+
+  # Allow CORS GET requests.
+  header {
+    destination = "http.access-control-allow-origin"
+    type        = "response"
+    action      = "set"
+    name        = "CORS Allow"
+    source      = "\"*\""
+  }
+
+  # Remove Google cookies from the origin
+  header {
+    destination = "http.x-goog-generation"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-generation"
+  }
+
+  header {
+    destination = "http.x-goog-metageneration"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-metageneration"
+  }
+
+  header {
+    destination = "http.x-guploader-uploadid"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-guploader-uploadid"
+  }
+
+  header {
+    destination = "http.x-goog-hash"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-hash"
+  }
+
+  header {
+    destination = "http.x-goog-meta-goog-reserved-file-mtime"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-meta-goog-reserved-file-mtime"
+  }
+
+  header {
+    destination = "http.x-goog-storage-class"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-storage-class"
+  }
+
+  header {
+    destination = "http.x-goog-stored-content-encoding"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-stored-content-encoding"
+  }
+
+  header {
+    destination = "http.x-goog-stored-content-length"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-goog-stored-content-length"
+  }
+
+  # Do not cache 'not found' & authenticated requests:
+  condition {
+    type      = "CACHE"
+    name      = "is-not-found"
+    statement = "beresp.status == 404"
+  }
+
+  cache_setting {
+    name            = "pass-not-found"
+    cache_condition = "is-not-found"
+    action          = "pass"
+    ttl             = 120
+    stale_ttl       = 120
+  }
+
+  request_setting {
+    name = "Force TLS"
+    force_ssl = true
+    xff = "leave"
+  }
+
+  vcl {
+    name    = "Main"
+    content = file("${path.module}/vcl/binaries.vcl")
+    main    = true
+  }
+
+  product_enablement {
+    origin_inspector = true
+    domain_inspector = true
+  }
+
+  force_destroy = true
+}
+
