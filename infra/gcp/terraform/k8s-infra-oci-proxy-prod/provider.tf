f5f92b649c6d5368e1ea665fb14d7c626efb32a4
Arnaud Meukam <ameukam@gmail.com>
2024-01-29 15:17:22 +0100

    registry.k8s.io: Rollout europe-west3 and europe-west10
    Related to:
      - https://github.com/kubernetes/registry.k8s.io/issues/236
    
    Rollout archeio in europe-west3 and europe-west10
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/k8s-infra-oci-proxy-prod/provider.tf
+++ infra/gcp/terraform/k8s-infra-oci-proxy-prod/provider.tf
@@ -29,11 +29,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.38.0"
+      version = "~> 4.84.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.38.0"
+      version = "~> 4.84.0"
     }
   }
 }
