6c12c890b7e7d2a845fa7e04cff43e880c108bef
Arnaud Meukam <ameukam@gmail.com>
2024-03-29 08:31:03 +0100

    GCP: fix calendar period for CAPG period
    Ensure the budget for CAPG cover the yearly period
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/k8s-infra-kubernetes-io/provider.tf
+++ infra/gcp/terraform/k8s-infra-kubernetes-io/provider.tf
@@ -30,11 +30,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.73.2"
+      version = "~> 4.84.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.73.2"
+      version = "~> 4.84.0"
     }
   }
 }
