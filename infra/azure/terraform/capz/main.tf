05a07066276d62bec2610f821c340c75a884d17d
ritikaguptams <ritikagupta@microsoft.com>
2024-07-30 09:12:44 -0700

    add role assignments for gmsa creds
    Signed-off-by: ritikaguptams <ritikagupta@microsoft.com>

--- infra/azure/terraform/capz/main.tf
+++ infra/azure/terraform/capz/main.tf
@@ -80,29 +80,9 @@ resource "azurerm_storage_account" "k8sprowstorage" {
   min_tls_version                  = "TLS1_0"
   account_replication_type         = "RAGRS"
   cross_tenant_replication_enabled = true
-  depends_on = [azurerm_resource_group.capz_ci]
-}
-
-# Import identities module
-module "identities" {
-  source              = "./identities"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  depends_on = [azurerm_resource_group.capz_ci]
-}
-
-# Import key vault module
-module "key_vault" {
-  source              = "./key-vault"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id  
-  identities = {
-    cloud_provider_user_identity_id = module.identities.cloud_provider_user_identity_id
-    domain_vm_identity_id           = module.identities.domain_vm_identity_id
-    gmsa_user_identity_id           = module.identities.gmsa_user_identity_id
-  }
-  depends_on = [azurerm_resource_group.capz_ci]
+  depends_on = [
+    azurerm_resource_group.capz_ci
+  ]
 }
 
 # Import container registry module
@@ -110,7 +90,36 @@ module "container_registry" {
   source              = "./container-registry"
   resource_group_name = var.resource_group_name
   location            = var.location
-  depends_on = [azurerm_resource_group.capz_ci]
+  depends_on = [
+    azurerm_resource_group.capz_ci
+  ]
+}
+
+# Import identities module
+module "identities" {
+  source                   = "./identities"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  subscription_id          = data.azurerm_client_config.current.subscription_id
+  container_registry_scope = module.container_registry.container_registry_id 
+  depends_on = [
+    azurerm_resource_group.capz_ci
+  ]
+}
+
+# Import key vault module
+module "key_vault" {
+  source                            = "./key-vault"
+  resource_group_name               = var.resource_group_name
+  location                          = var.location
+  tenant_id                         = data.azurerm_client_config.current.tenant_id  
+  identities = {
+    domain_vm_identity_id           = module.identities.domain_vm_identity_id
+    gmsa_user_identity_id           = module.identities.gmsa_user_identity_id
+  }
+  depends_on = [
+    azurerm_resource_group.capz_ci
+  ]
 }
 
 # Import role assignments module
@@ -120,6 +129,7 @@ module "role_assignments" {
   container_registry_scope = module.container_registry.container_registry_id
   storage_account_scope    = azurerm_storage_account.k8sprowstorage.id
   subscription_id          = data.azurerm_client_config.current.subscription_id 
+  key_vault_id             = module.key_vault.key_vault_id
   depends_on = [
     azurerm_resource_group.capz_ci,
     azurerm_storage_account.k8sprowstorage,
