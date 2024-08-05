62d61716b5f7e1b48c1093c5244ca1594602e681
upodroid <upodroid@users.noreply.github.com>
2024-08-05 16:45:23 +0100

    gke arm64 nodes

--- infra/gcp/terraform/k8s-infra-prow-build/main.tf
+++ infra/gcp/terraform/k8s-infra-prow-build/main.tf
@@ -100,3 +100,21 @@ module "prow_build_nodepool_n1_highmem_8_localssd" {
   service_account           = module.prow_build_cluster.cluster_node_sa.email
 }
 
+module "prow_build_nodepool_t2a_standard_8" {
+  source        = "../modules/gke-nodepool"
+  project_name  = module.project.project_id
+  cluster_name  = module.prow_build_cluster.cluster.name
+  location      = module.prow_build_cluster.cluster.location
+  name          = "pool5-arm64"
+  initial_count = 1
+  min_count     = 1
+  max_count     = 10
+  image_type    = "UBUNTU_CONTAINERD"
+  machine_type  = "t2a-standard-8"
+  disk_size_gb  = 150
+  disk_type     = "pd-ssd"
+  // GKE automatically taints arm64 nodes
+  // https://cloud.google.com/kubernetes-engine/docs/how-to/prepare-arm-workloads-for-deployment#overview
+  # ephemeral_local_ssd_count = 2 # each is 375GB
+  service_account = module.prow_build_cluster.cluster_node_sa.email
+}
