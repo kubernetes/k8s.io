e9d1a5ab0343aac9baf6d7c8c391bfa3c31b90a9
Mahamed Ali <cy@borg.dev>
2024-08-15 13:15:18 +0100

    Bootstrap the new clusters in k8s-infra-prow project. (#7127)
    * deploy argocd
    
    * manage argocd via argocd
    
    * configure app of apps
    
    * fix sync issue
    
    * bump argocd
    
    * create eso clusterstore
    
    * deploy nginx ingress
    
    * set the dualstack address correctly
    
    * force dualstack
    
    * fix diff
    
    * configure issuer
    
    * switch the repository to the kubernetes one
    
    ---------
    
    Co-authored-by: upodroid <upodroid@users.noreply.github.com>

---
+++ infra/gcp/terraform/kubernetes-public/iam.tf
@@ -0,0 +1,31 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+module "iam" {
+  source  = "terraform-google-modules/iam/google//modules/projects_iam"
+  version = "~> 7"
+
+  projects = ["kubernetes-public"]
+
+  mode = "authoritative"
+
+  bindings = {
+    "roles/secretmanager.secretAccessor" = [
+      "serviceAccount:kubernetes-external-secrets@kubernetes-public.iam.gserviceaccount.com",
+      "principal://iam.googleapis.com/projects/16065310909/locations/global/workloadIdentityPools/k8s-infra-prow.svc.id.goog/subject/ns/external-secrets/sa/external-secrets",
+    ]
+  }
+}
