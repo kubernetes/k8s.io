5841da46fb7e44de2c387bbe08a7e45eb45acdbe
upodroid <upodroid@users.noreply.github.com>
2024-08-29 15:29:21 +0100

    fix eks prow build cluster IAM changes

--- infra/aws/terraform/prow-build-cluster/iam.tf
+++ infra/aws/terraform/prow-build-cluster/iam.tf
@@ -28,28 +28,7 @@ resource "aws_iam_role" "eks_prow_admin" {
       {
         "Effect" : "Allow",
         "Principal" : {
-          "Federated" : aws_iam_openid_connect_provider.k8s_prow[0].arn
-        },
-        "Action" : "sts:AssumeRoleWithWebIdentity",
-        "Condition" : {
-          "StringEquals" : {
-            "container.googleapis.com/v1/projects/k8s-prow/locations/us-central1-f/clusters/prow:sub" : [
-              // https://github.com/kubernetes/test-infra/tree/master/config/prow/cluster
-              // all services that load kubeconfig should be listed here
-              "system:serviceaccount:default:deck",
-              "system:serviceaccount:default:config-bootstrapper",
-              "system:serviceaccount:default:crier",
-              "system:serviceaccount:default:sinker",
-              "system:serviceaccount:default:prow-controller-manager",
-              "system:serviceaccount:default:hook"
-            ]
-          }
-        }
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Federated" : aws_iam_openid_connect_provider.gke_utility_cluster[0].arn
+          "Federated" : aws_iam_openid_connect_provider.gke_utility_cluster.arn
         },
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
