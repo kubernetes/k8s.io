6137c009fc5a5752f2fc99a24dda64bffadf69e1
Arnaud Meukam <ameukam@gmail.com>
2024-09-16 16:08:55 +0200

    Bump GCP provider

--- infra/gcp/terraform/k8s-infra-releases-prod/providers.tf
+++ infra/gcp/terraform/k8s-infra-releases-prod/providers.tf
@@ -29,11 +29,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.39.0"
+      version = "~> 5.44.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.39.0"
+      version = "~> 5.44.0"
     }
   }
 }
