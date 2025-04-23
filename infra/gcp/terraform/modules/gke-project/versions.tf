4bb26c36afd32c93594c2140ba2e8764d1cf8c58
Arnaud Meukam <ameukam@gmail.com>
2025-04-23 23:03:40 +0200

    GCP: bump terraform providers
    Bump the GCP provider so we can use external terraform modules requiring
    the 6.x provider
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/gcp/terraform/modules/gke-project/versions.tf
+++ infra/gcp/terraform/modules/gke-project/versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.45.0"
+      version = "~> 6.31.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.45.0"
+      version = "~> 6.31.0"
     }
   }
 }
