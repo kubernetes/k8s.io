05a54e5dca103f5b74535b54f602ea2b4eb69ff5
Christian Schlotter <christian.schlotter@broadcom.com>
2025-05-20 12:29:39 +0200

    gcve: terraform comment cleanups

--- infra/gcp/terraform/k8s-infra-gcp-gcve/vsphere/projects.tf
+++ infra/gcp/terraform/k8s-infra-gcp-gcve/vsphere/projects.tf
@@ -18,21 +18,6 @@ locals {
   gcp_gcve_project_name = "k8s-infra-e2e-gcp-gcve-project"
 }
 
-# ## create global permissions to read cluster, datastore, host information etc.
-# module "cluster-api-provider-vsphere-iam" {
-#   source = "./modules/vsphere-project-iam"
-
-#   group = var.cluster_api_provider_vsphere_iam_group
-#   vsphere_role_id = vsphere_role.vsphere-ci.id
-#   vsphere_datacenter_id = data.vsphere_datacenter.datacenter.id
-#   vsphere_datastore_id = data.vsphere_datastore.datastore.id
-#   vsphere_compute_cluster_id = data.vsphere_compute_cluster.compute_cluster.id
-#   vsphere_resource_pool_id = vsphere_resource_pool.prow.id
-#   vsphere_network_name = var.vsphere_network_name
-
-#   depends_on = [ vsphere_role.vsphere-ci, vsphere_resource_pool.prow ]
-# }
-
 ## create the projects (resource pool, folder, assign permissions per resource pool and folder)
 module "gcp-gcve-projects" {
   source = "./modules/gcp-gcve-project"
