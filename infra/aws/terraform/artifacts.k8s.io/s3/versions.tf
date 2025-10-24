18acf6b911d3df1ea913d4037da3f4677c67f4cf
Mahamed Ali <cy@borg.dev>
2025-10-25 01:10:55 +0300

    configure artifacts.k8s.io to use atlantis + atlantis IAM changes in management account (#8686)

--- infra/aws/terraform/artifacts.k8s.io/s3/versions.tf
+++ infra/aws/terraform/artifacts.k8s.io/s3/versions.tf
@@ -19,8 +19,7 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
     }
   }
 }
--- infra/aws/terraform/management-account/provider.tf
+++ infra/aws/terraform/management-account/provider.tf
@@ -30,7 +30,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.93.0"
+      version = "~> 6.17.0"
     }
   }
 }
