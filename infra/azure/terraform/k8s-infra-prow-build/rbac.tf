9b08bfd344f18df0630e62603e3ebc83a95ad51f
Mahamed Ali <cy@borg.dev>
2026-01-06 23:26:55 +0300

    rebuild the AKS build cluster (#8911)

--- infra/azure/terraform/k8s-infra-prow-build/rbac.tf
+++ infra/azure/terraform/k8s-infra-prow-build/rbac.tf
@@ -34,3 +34,48 @@ resource "azurerm_role_assignment" "kubelet_mi_operator" {
   scope                = azurerm_resource_group.rg.id
   principal_id         = azurerm_user_assigned_identity.aks_kubelet_identity.principal_id
 }
+
+// aks-admin managed identity
+resource "azurerm_user_assigned_identity" "aks_admin" {
+  location            = azurerm_resource_group.rg.location
+  name                = "aks-admin"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_federated_identity_credential" "aks_admin_argocd" {
+  for_each = toset([
+    "system:serviceaccount:argocd:argocd-application-controller",
+    "system:serviceaccount:argocd:argocd-server",
+  ])
+  name                = "argocd-${reverse(split(":", each.key))[0]}"
+  resource_group_name = azurerm_resource_group.rg.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://container.googleapis.com/v1/projects/k8s-infra-prow/locations/us-central1/clusters/utility"
+  parent_id           = azurerm_user_assigned_identity.aks_admin.id
+  subject             = each.key
+}
+
+resource "azurerm_federated_identity_credential" "aks_admin_prow" {
+  // https://github.com/kubernetes/k8s.io/tree/main/kubernetes/gke-prow/prow
+  // all services that load kubeconfig should be listed here
+  for_each = toset([
+    "system:serviceaccount:default:deck",
+    "system:serviceaccount:default:config-bootstrapper",
+    "system:serviceaccount:default:crier",
+    "system:serviceaccount:default:sinker",
+    "system:serviceaccount:default:prow-controller-manager",
+    "system:serviceaccount:default:hook",
+  ])
+  name                = "prow-${reverse(split(":", each.key))[0]}"
+  resource_group_name = azurerm_resource_group.rg.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://container.googleapis.com/v1/projects/k8s-infra-prow/locations/us-central1/clusters/prow"
+  parent_id           = azurerm_user_assigned_identity.aks_admin.id
+  subject             = each.key
+}
+
+resource "azurerm_role_assignment" "aks_admin" {
+  role_definition_name = "Azure Arc Kubernetes Cluster Admin"
+  scope                = azurerm_resource_group.rg.id
+  principal_id         = azurerm_user_assigned_identity.aks_kubelet_identity.principal_id
+}
