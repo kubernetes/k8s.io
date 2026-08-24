70ac95c2e3936f0e9d2f30e45a041c1d019f9984
upodroid <cy@borg.dev>
2026-08-24 20:22:57 +0300

    switch to using fastly cdn for registy.k8s.io

---
+++ infra/fastly/terraform/cdn.registry.k8s.io/main.tf
@@ -0,0 +1,35 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+module "cdn" {
+  source = "../cdn"
+  domain = "cdn.registry.k8s.io"
+  datadog_config = {
+    token        = data.google_secret_manager_secret_version_access.datadog_api_key.secret_data,
+    service_name = "cdn.registry.k8s.io",
+    env          = "prod",
+  }
+  cloud     = "aws"
+  cache_ttl = 2592000 # 30 days
+  bucket_configs = [
+    {
+      name   = "prod-registry-k8s-io-us-east-2",
+      region = "us-east-2",
+      public = true
+    },
+  ]
+}
