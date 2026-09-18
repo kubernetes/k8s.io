b07191355acf5616c2c2e569913281b629de7449
Matthew Cary <mattcary@google.com>
2026-09-18 16:55:06 +0000

    Update with improve generate.sh

--- registry.k8s.io/images/k8s-staging-cloud-provider-gcp/generate.sh
+++ registry.k8s.io/images/k8s-staging-cloud-provider-gcp/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2023 The Kubernetes Authors.
+# Copyright 2026 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,20 +20,49 @@ set -o pipefail
 
 readonly repo="gcr.io/k8s-staging-cloud-provider-gcp"
 readonly tag_filter="tags~^v\d+\.\d+\.\d+\$"
-# List of repos under https://console.cloud.google.com/gcr/images/k8s-staging-cloud-provider-gcp/GLOBAL
-readonly images=(
-    cloud-controller-manager
-    gcp-compute-persistent-disk-csi-driver
-    gcp-filestore-csi-driver
-)
 
-for image in "${images[@]}"; do
+# Pick up images from current image file. This should be bootstrapped
+# from https://console.cloud.google.com/gcr/images/k8s-staging-cloud-provider-gcp/GLOBAL.
+
+num_images=$(yq '. | length' images.yaml)
+rm -f _new_images.yaml
+for ((i=0; i<num_images; i++)); do
+  image=$(yq ".[$i].name" images.yaml)
+  existing="_${image}_existing"
+
+  yq ".[$i]" images.yaml \
+    | grep sha256 \
+    | while read -r _ tag; do echo "$tag"; done \
+    > "$existing"
+  
+  {
     echo "- name: ${image}"
     echo "  dmap:"
-    gcloud container images list-tags \
+    # Copy over existing lines.
+    yq ".[$i]" images.yaml | grep sha256 \
+      | while read -r digest tag; do
+          # The digest contains a colon already
+          echo "    ${digest} ${tag}"
+        done
+  } >> _new_images.yaml
+
+  # Find any new image tags not already in the file.
+  # For older tags, the digests in the registry may not match
+  # what have already been promoted.
+  gcloud container images list-tags \
         "${repo}/$image" \
         --format="get(digest, tags)" \
         --sort-by="tags" \
         --filter="${tag_filter}" \
-        | sed -e 's/\([^ ]*\)\t\(.*\)/    "\1": ["\2"]/'
-done > images.yaml
+        | while read -r digest tag; do
+    if echo "$tag" | grep -q v999; then
+      # Skip experimental releases (v999.x)
+      continue
+    fi
+    if ! grep -Fq "$tag" "$existing"; then
+      echo "    \"${digest}\": [\"${tag}\"]" >> _new_images.yaml
+    fi
+  done
+  rm -f "$existing"
+done
+mv _new_images.yaml images.yaml
